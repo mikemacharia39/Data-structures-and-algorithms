8daac299ee6cebb957fc147d9daabcb76a6cae29
mikemacharia39 <mikemacharia39@gmail.com>
2021-08-15 20:30:55 +0300

    Gorilla tests and mysql tests

---
+++ src/leetTests/StringManipulation/TestGorrilla.java
@@ -0,0 +1,48 @@
+package leetTests.StringManipulation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestGorrilla {
+    public static String[] sorted( String[] events ) {
+        final java.time.format.DateTimeFormatter formatter = java.time.format.DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        final java.time.LocalDate now = java.time.LocalDate.of(2000, 1, 1);
+
+        java.util.List<String> listOfEvents = new java.util.ArrayList<String>();
+        for(String event: events) {
+            java.util.Optional<java.time.LocalDate> date = findDateInLine(event, formatter);
+            //if(!date.isPresent()) continue;
+
+            java.time.Period yearsLeft = java.time.Period.between(date.get(),now);
+
+            int dateIndex = event.indexOf(date.get().format(formatter));
+            String title = event.substring(0, dateIndex - 2);
+            StringBuilder sb = new StringBuilder();
+            sb.append(yearsLeft.getYears()).append("years;");
+            sb.append(yearsLeft.getMonths()).append("months;");
+            sb.append(yearsLeft.getDays()).append("days;").append("-").append(title);
+
+            listOfEvents.add(sb.toString());
+        }
+        listOfEvents.sort((String e1, String e2)->e1.compareTo(e2));
+
+        return listOfEvents.toArray(new String[listOfEvents.size()]);
+    }
+    private static java.util.Optional<java.time.LocalDate> findDateInLine(String line,
+                                                                          java.time.format.DateTimeFormatter formatter) {
+        String regex = "\\d{1,2}-\\d{1,2}-\\d{4}";
+        java.util.regex.Matcher m = java.util.regex.Pattern.compile(regex).matcher(line);
+        if (m.find()) {
+            java.time.LocalDate date = java.time.LocalDate.parse(m.group(), formatter);
+            return java.util.Optional.of(date);
+        }
+        return java.util.Optional.empty();
+    }
+
+    public static void main(String[] args) {
+
+        String[] events = {"The date is 30-12-1963"};
+        System.out.println(Arrays.toString(TestGorrilla.sorted(events)));
+    }
+}

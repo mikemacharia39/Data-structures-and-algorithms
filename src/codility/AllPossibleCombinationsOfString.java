141835feecb7e1663e9b9c42752f4b498ff3f432
mikemacharia39 <mikemacharia39@gmail.com>
2022-01-16 21:07:31 +0300

    all combinations of a string

---
+++ src/codility/AllPossibleCombinationsOfString.java
@@ -0,0 +1,35 @@
+package codility;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllPossibleCombinationsOfString {
+    List<String> stringList = new ArrayList<>();
+    StringBuilder sb = new StringBuilder();
+
+    public List<String> combinations(String s) {
+        return combinations(s, 0);
+    }
+
+    public List<String> combinations(String s, int start) {
+        int len = s.length();
+        for (int i = start; i < len; i++) {
+            sb.append(s.charAt(i));
+            stringList.add(sb.toString());
+            combinations(s, i+1);
+            // In the first run length = 1, when you minus 1, you get 0, meaning we shall not append any character
+            // in the second run length = 2, when you minus 1, you get 1, append the next character
+            sb.setLength(sb.length() - 1);
+        }
+
+        return stringList;
+    }
+
+    public static void main(String[] args) {
+        String s = "WXYZ";
+
+        AllPossibleCombinationsOfString possibleCombinationsOfString = new AllPossibleCombinationsOfString();
+
+        System.out.println(possibleCombinationsOfString.combinations(s));
+    }
+}

ea01cee9c9e3578242aaf4c78528d37e89a04816
mikemacharia39 <mikemacharia39@gmail.com>
2022-04-20 21:26:05 +0300

    filter with streams

--- src/general/StreamingConcepts.java
+++ src/general/StreamingConcepts.java
@@ -6,6 +6,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class StreamingConcepts {
 
@@ -38,8 +39,30 @@ public class StreamingConcepts {
         Logger.getLogger(StreamingConcepts.class.getName()).log(Level.INFO, "Smallest value is: {0}", smallestVal);
     }
 
+    public void maxEvenNumberInList() {
+        List<Integer> numberList = Arrays.asList(3,9,1,12,17,2,4);
+
+        int maxEvenNum = numberList.stream()
+                                   .filter(value -> value%2 == 0)
+                                   .max(Integer::compare)
+                                   .get();
+        Logger.getLogger(StreamingConcepts.class.getName()).log(Level.INFO, "Max even number value is: {0}", maxEvenNum);
+    }
+
+    public void evenNumbersInList() {
+        List<Integer> numberList = Arrays.asList(3,9,1,12,17,2,4);
+
+        List<Integer> evenNums = numberList.stream()
+                                           .filter(n -> n%2 ==0)
+                                           .sorted()
+                                           .collect(Collectors.toList());
+        Logger.getLogger(StreamingConcepts.class.getName()).log(Level.INFO, "Max even number value is: {0}", evenNums);
+    }
+
     public static void main(String[] args) {
         StreamingConcepts streamingConcepts = new StreamingConcepts();
         streamingConcepts.minAndMaxValueInArray();
+        streamingConcepts.maxEvenNumberInList();
+        streamingConcepts.evenNumbersInList();
     }
 }

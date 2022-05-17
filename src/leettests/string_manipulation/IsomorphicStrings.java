a8040ea93b9f10f6f7724e6e3e4d06e19ae6eb66
mikemacharia39 <mikemacharia39@gmail.com>
2022-05-17 21:43:20 +0300

    solution to isomorphic strings

--- src/leettests/string_manipulation/IsomorphicStrings.java
+++ src/leettests/string_manipulation/IsomorphicStrings.java
@@ -1,4 +1,32 @@
 package leettests.string_manipulation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int len = s.length();
+        Map<Character, Character> map = new HashMap<>();
+
+        for(int i = 0; i < len; i++) {
+            if (map.containsKey(s.charAt(i))) {
+                if (map.get(s.charAt(i)) != t.charAt(i)) {
+                    return false;
+                }
+            } else if (map.containsValue(t.charAt(i))) {
+                if (!map.containsKey(s.charAt(i))) {
+                    return false;
+                }
+            } else {
+                map.put(s.charAt(i), t.charAt(i));
+            }
+        }
+
+        return true;
+    }
 }

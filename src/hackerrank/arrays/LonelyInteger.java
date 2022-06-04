048ba1155491f3767a85ed8941cca631f0638611
mikemacharia39 <mikemacharia39@gmail.com>
2022-06-04 17:59:07 +0300

    lonely integer solution

---
+++ src/hackerrank/arrays/LonelyInteger.java
@@ -0,0 +1,23 @@
+package hackerrank.arrays;
+
+/**
+ * Given an array of integers, where all elements but one occur twice, find the unique element.
+ *
+ * Example
+ * a = {1,2,3,4,3,2,1}
+ *
+ * The unique element is 4.
+ *
+ * Function Description
+ *
+ * Complete the lonely integer function in the editor below.
+ *
+ * lonely integer has the following parameter(s):
+ *
+ * int a[n]: an array of integers
+ *
+ * Returns int: the element that occurs only once
+ */
+public class LonelyInteger {
+
+}

8e64a0ad360d59f33c6e4acd9f6fffad484d8515
mikemacharia39 <mikemacharia39@gmail.com>
2022-06-24 19:14:22 +0300

    solution to shuffle string

--- src/leettests/arrays/ShuffleString.java
+++ src/leettests/arrays/ShuffleString.java
@@ -19,5 +19,11 @@ package leettests.arrays;
  * Explanation: After shuffling, each character remains in its position.
  */
 public class ShuffleString {
-
+    public String restoreString(String s, int[] indices) {
+        char[] answer = new char[indices.length];
+        for (int i = 0; i < indices.length; i++) {
+            answer[indices[i]] = s.charAt(i);
+        }
+        return new String(answer);
+    }
 }

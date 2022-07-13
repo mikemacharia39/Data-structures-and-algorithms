b81a788df7a24c31d41cae94a6a12e2f81ac5f8b
mikemacharia39 <mikemacharia39@gmail.com>
2022-07-13 21:49:08 +0300

    2 solutions to check if string is shift of another, add validation

--- src/general/ShiftStrings.java
+++ src/general/ShiftStrings.java
@@ -14,6 +14,10 @@ public class ShiftStrings {
      * @return true|false
      */
     public boolean isShifted(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
         if ((s1 + s1).contains(s2)) {
             return true;
         }

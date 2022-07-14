90861db4bc4845acaea145dfb0e312d2fbeee2e3
mikemacharia39 <mikemacharia39@gmail.com>
2022-07-14 20:41:17 +0300

    define question

---
+++ src/leettests/linkedLists/ReverseLinkedList.java
@@ -0,0 +1,13 @@
+package leettests.linkedLists;
+
+/**
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ * Example 1:
+ * Input: head = [1,2,3,4,5]
+ * Output: [5,4,3,2,1]
+ */
+public class ReverseLinkedList {
+
+
+}

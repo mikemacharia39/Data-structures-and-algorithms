486db0ac041770ce289f077c258658bc1d891679
mikemacharia39 <mikemacharia39@gmail.com>
2022-07-14 20:43:37 +0300

    solution1 to reversing

--- src/leettests/linkedLists/ReverseLinkedList.java
+++ src/leettests/linkedLists/ReverseLinkedList.java
@@ -1,5 +1,10 @@
 package leettests.linkedLists;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Given the head of a singly linked list, reverse the list, and return the reversed list.
 
@@ -9,5 +14,39 @@ package leettests.linkedLists;
  */
 public class ReverseLinkedList {
 
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode (int val) {
+            this.val = val;
+        }
+    }
+
+    /**
+     * Brute force
+     * @param head ListNode
+     * @return ListNode
+     */
+    public ListNode reverseList(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+
+        Collections.reverse(list);
+
+        Iterator<Integer> it = list.iterator();
+
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        while (it.hasNext()) {
+            ListNode node = new ListNode(it.next());
+            curr.next = node;
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
 
 }

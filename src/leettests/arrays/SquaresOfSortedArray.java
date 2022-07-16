f8f2d14a6e35b3218f359940f69828fa905401be
mikemacharia39 <mikemacharia39@gmail.com>
2022-07-16 12:50:43 +0300

    extra suggestions

--- src/leettests/arrays/SquaresOfSortedArray.java
+++ src/leettests/arrays/SquaresOfSortedArray.java
@@ -2,21 +2,21 @@ package leettests.arrays;
 
 /**
  * <a href="https://leetcode.com/problems/squares-of-a-sorted-array/">...</a>
- *
+ * <p>
  * Given an integer array nums sorted in non-decreasing order, return an array of the squares of
  * each number sorted in non-decreasing order.
- *
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: nums = [-4,-1,0,3,10]
  * Output: [0,1,9,16,100]
  * Explanation: After squaring, the array becomes [16,1,0,9,100].
  * After sorting, it becomes [0,1,9,16,100].
- *
+ * <p>
  * Example 2:
  * Input: nums = [-7,-3,2,3,11]
  * Output: [4,9,9,49,121]
- *
+ * <p>
  * **Follow up**: Squaring each element and sorting the new array is very trivial,
  * could you find an O(n) solution using a different approach?
  */
@@ -38,6 +38,7 @@ public class SquaresOfSortedArray {
         int k = 0;
         while (endNegative >= 0 && beginPositive < n) {
             if (nums[endNegative] * nums[endNegative] > nums[beginPositive] * nums[beginPositive]) {
+            // if (Math.abs(nums[endNegative]) > Math.abs(nums[beginPositive])) {  // you can chose to compare this way too
                 ans[k] = nums[beginPositive] * nums[beginPositive];
                 beginPositive++;
             } else {

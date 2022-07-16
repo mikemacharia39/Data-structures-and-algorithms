e704936c85e7fade56087ddeef0aa68ef834068d
mikemacharia39 <mikemacharia39@gmail.com>
2022-07-16 21:31:54 +0300

    add binary solution

--- src/leettests/string_manipulation/AddBinary.java
+++ src/leettests/string_manipulation/AddBinary.java
@@ -1,6 +1,8 @@
 package leettests.string_manipulation;
 
 /**
+ * <a href="https://leetcode.com/problems/add-binary/">...</a>
+ *
  * Given two binary strings a and b, return their sum as a binary string.
  *
  * Example 1:
@@ -14,4 +16,35 @@ package leettests.string_manipulation;
  * Output: "10101"
  */
 public class AddBinary {
+
+    public String addBinary(String a, String b) {
+
+        int carry = 0;
+        int alen = a.length()-1;
+        int blen = b.length()-1;
+
+        StringBuilder sb = new StringBuilder();
+        while (alen >= 0 || blen >= 0) {
+
+            if (alen >= 0) {
+                carry += a.charAt(alen) - '0';
+                alen--;
+            }
+
+            if (blen >= 0) {
+                carry += b.charAt(blen) - '0';
+                blen--;
+            }
+
+            sb.insert(0, (carry%2));
+
+            carry = carry/2;
+        }
+
+        if (carry == 1) {
+            sb.insert(0, carry);
+        }
+
+        return sb.toString();
+    }
 }

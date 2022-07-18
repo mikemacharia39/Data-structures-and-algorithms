cbcf2aa3b349f976937732ae79ca29c6283d09de
mikemacharia39 <mikemacharia39@gmail.com>
2022-07-18 21:17:07 +0300

    find the middle of a linked list

--- src/leettests/linkedLists/PalindromeLinkedList.java
+++ src/leettests/linkedLists/PalindromeLinkedList.java
@@ -28,4 +28,26 @@ public class PalindromeLinkedList {
     public boolean isPalindrome(ListNode head) {
 
     }
+
+    /**
+     * To find the middle of the linked list
+     * To find the middle, use 2 pointer:
+     * A slow pointer moving step by step and a fast pointer moving 2 times faster than the slow pointer
+     *
+     * @param head root
+     * @return mid
+     */
+    public ListNode mid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+
 }

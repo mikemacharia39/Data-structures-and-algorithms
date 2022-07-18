5aae470a63d5f07df11da7fd4896f487a28e2af0
mikemacharia39 <mikemacharia39@gmail.com>
2022-07-18 21:04:55 +0300

    define is palindrome problem

---
+++ src/leettests/linkedLists/PalindromeLinkedList.java
@@ -0,0 +1,31 @@
+package leettests.linkedLists;
+
+/**
+ * Given the head of a singly linked list, return true if it is a palindrome.
+ *
+ * Example 1:
+ *
+ * Input: head = [1,2,2,1]
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ * Input: head = [1,2]
+ * Output: false
+ */
+public class PalindromeLinkedList {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public boolean isPalindrome(ListNode head) {
+
+    }
+}

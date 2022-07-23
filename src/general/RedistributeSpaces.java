91e95dca9a8d763fb0182605033d71efafcb2630
mikemacharia39 <mikemacharia39@gmail.com>
2022-07-23 15:31:00 +0300

    description of question

--- src/general/RedistributeSpaces.java
+++ src/general/RedistributeSpaces.java
@@ -2,6 +2,25 @@ package general;
 
 import java.util.StringTokenizer;
 
+/**
+ * Input: fixed-size array of characters, containing:
+ *
+ * 0 or more leading spaces
+ * 0 or more words separated by 1 or more spaces
+ * 0 or more trailing spaces
+ * Output: the same array where spaces are re-distributed between the words in such a way that they are:
+ *
+ * no more leading spaces
+ * no more trailing spaces
+ * roughly the same space gaps between the words. i.e. the minimum and the maximum number of spaces
+ * between the words can not differ by more than 1.
+ * Restriction: in-place algorithm. Do not use additional space for copying the characters.
+ *
+ * Example:
+ * Input: "....word1.....word2.....word3....."
+ * Output: "word1.........word2..........word3"
+ * Gaps: 10 and 9
+ */
 public class RedistributeSpaces {
 
 

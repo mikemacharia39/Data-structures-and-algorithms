95b024c2a7e7f5f328e8dceeb84c0aedda1ab675
mikemacharia39 <mikemacharia39@gmail.com>
2022-07-23 16:06:18 +0300

    solution 1 of redistributing spaces

--- src/general/RedistributeSpaces.java
+++ src/general/RedistributeSpaces.java
@@ -1,7 +1,5 @@
 package general;
 
-import java.util.StringTokenizer;
-
 /**
  * Input: fixed-size array of characters, containing:
  *
@@ -25,21 +23,39 @@ public class RedistributeSpaces {
 
 
     public static String newWord(String word) {
-
-        int numOfWords = numOfWords(word);
         int numOfSpaces = numOfSpaces(word);
+        String[] words = word.split("\\s+");
+        int numOfWords = words.length;
 
+        System.out.println("Num of spaces: " + numOfSpaces + " | words: " + numOfWords);
 
+        // to equally redistribute spaces
+        int div = numOfSpaces/(numOfWords-1);
 
-        return word;
+        //to find the remaining spaces
+        int rem = numOfSpaces%(numOfWords-1);
+
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+        for(String str : words) {
+            int extra = rem > 0 ? 1 : 0;
+            sb.append(str);
+            if (count < words.length) { // to prevent spaces at the end
+                sb.append(" ".repeat(div + extra));
+            }
+            count++;
+            rem--;
+        }
+
+        return sb.toString();
     }
 
     private static int numOfWords(String word) {
         if (word == null || word.isEmpty()) {
             return 0;
         }
-        StringTokenizer tokens = new StringTokenizer(word);
-        return tokens.countTokens();
+
+        return 0;
     }
 
     private static int numOfSpaces(String word) {
@@ -58,6 +74,6 @@ public class RedistributeSpaces {
         String word = "word1     word2     word3   word4 word5 "; //"....word1.....word2.....word3.....";
         //String word = "word1     word2     word3"; //"....word1.....word2.....word3.....";
 
-        RedistributeSpaces.newWord(word);
+        System.out.println(RedistributeSpaces.newWord(word));
     }
 }

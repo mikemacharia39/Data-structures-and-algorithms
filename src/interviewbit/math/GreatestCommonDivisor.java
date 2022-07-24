1c73871b42f90aa2c23e6f969d99ff1a12707c6b
mikemacharia39 <mikemacharia39@gmail.com>
2022-07-24 16:33:35 +0300

    solution of greatest common divisor, description

--- src/interviewbit/math/GreatestCommonDivisor.java
+++ src/interviewbit/math/GreatestCommonDivisor.java
@@ -1,5 +1,17 @@
 package interviewbit.math;
 
+/**
+ * Given 2 non-negative integers m and n, find gcd(m, n)
+ *
+ * GCD of 2 integers m and n is defined as the greatest integer g such that g is a divisor of both m and n.
+ *
+ * Both m and n fit in a 32 bit signed integer.
+ *
+ * Example
+ *
+ * m : 6
+ * n : 9
+ */
 public class GreatestCommonDivisor {
     public int gcd(int A, int B) {
         int l = lesser(A, B);

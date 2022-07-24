0984d2b8d4f11978e72d7e972b21b25c5a838c88
mikemacharia39 <mikemacharia39@gmail.com>
2022-07-24 16:51:21 +0300

    solution 2 using the euclidean approach

--- src/interviewbit/math/GreatestCommonDivisor.java
+++ src/interviewbit/math/GreatestCommonDivisor.java
@@ -15,7 +15,46 @@ package interviewbit.math;
  * n : 9
  */
 public class GreatestCommonDivisor {
+
+    /**
+     * The Euclidean Algorithm
+     * <a href="https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm">...</a>
+     * @param A val 1
+     * @param B val 2
+     * @return GCD
+     */
     public int gcd(int A, int B) {
+        int l = Math.min(A, B);
+
+        if (l == 0) {
+            return Math.max(A, B);
+        }
+
+        if (l < 2 ) {
+            return 1;
+        }
+
+        int diff = Math.abs(A - B);
+
+        if (diff == 0) {
+            return A;
+        }
+
+        for (int i = diff; i > 1; i--) {
+            if (A%i == 0 && B%i == 0) {
+                return i;
+            }
+        }
+        return 1;
+    }
+
+    /**
+     * GCD
+     * @param A val 1
+     * @param B val 2
+     * @return GCD
+     */
+    public int gcdSln(int A, int B) {
         int l = lesser(A, B);
 
         if (l == 0) {

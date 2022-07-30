2adeef100a55a2421489e327e7cc422e5844617e
mikemacharia39 <mikemacharia39@gmail.com>
2022-07-30 20:21:31 +0300

    valid perfect square link to question

--- src/leettests/math/ValidPerfectSquare.java
+++ src/leettests/math/ValidPerfectSquare.java
@@ -1,6 +1,8 @@
 package leettests.math;
 
 /**
+ * <a href="https://leetcode.com/problems/valid-perfect-square/">...</a>
+ *
  * Given a positive integer num, write a function which returns True if num is a perfect square else False.
  * Follow up: Do not use any built-in library function such as sqrt.
  *

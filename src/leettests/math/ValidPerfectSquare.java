3bb7d09aae2f3ea8fd4fc7906d4d8b76ecf999c3
mikemacharia39 <mikemacharia39@gmail.com>
2022-07-30 20:21:03 +0300

    valid perfect square description

--- src/leettests/math/ValidPerfectSquare.java
+++ src/leettests/math/ValidPerfectSquare.java
@@ -1,4 +1,29 @@
 package leettests.math;
 
+/**
+ * Given a positive integer num, write a function which returns True if num is a perfect square else False.
+ * Follow up: Do not use any built-in library function such as sqrt.
+ *
+ * Example 1:
+ *
+ * Input: num = 16
+ * Output: true
+ *
+ * Example 2:
+ *
+ * Input: num = 14
+ * Output: false
+ *
+ * Example 3:
+ *
+ * Input: num = 1
+ * Output: true
+ *
+ * Example 4:
+ *
+ * Input: num = 2147483647
+ * Output: false
+ */
 public class ValidPerfectSquare {
+
 }

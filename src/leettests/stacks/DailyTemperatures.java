063893830d04c5bd5a06ce0e1d93b337c70c18af
mikemacharia39 <mikemacharia39@gmail.com>
2022-08-07 20:31:48 +0300

    daily temperatures using other solutions

--- src/leettests/stacks/DailyTemperatures.java
+++ src/leettests/stacks/DailyTemperatures.java
@@ -1,5 +1,6 @@
 package leettests.stacks;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 /**
@@ -52,5 +53,40 @@ public class DailyTemperatures {
         }
 
         return result;
+    }
+
+    /**
+     * Better solution
+     */
+    public int[] dailyTemperatures1(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        for (int i = temperatures.length - 1; i >= 0; i--) {
+            for (int j = i - 1; j >= 0 && temperatures[j] < temperatures[i]; j--) {
+                result[j] = i - j;
+            }
+        }
+        return result;
+    }
+
+
+
+    /**
+     * Exceeded time limit
+     */
+    public int[] dailyTemperatures2(int[] temperatures) {
+
+        int len = temperatures.length;
+        int[] answer = new int[len];
+        Arrays.fill(answer, 0);
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (temperatures[i] < temperatures[j]) {
+                    answer[i] = j - i;
+                    break;
+                }
+            }
+        }
+
+        return answer;
     }
 }

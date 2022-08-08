ff5311b75190d6b9d94af901ac5c3ac6edcf0048
mikemacharia39 <mikemacharia39@gmail.com>
2022-08-08 20:51:44 +0300

    define question intersection of two linked lists link

--- src/leettests/linkedLists/IntersectionOfTwoLinkedLists.java
+++ src/leettests/linkedLists/IntersectionOfTwoLinkedLists.java
@@ -1,6 +1,8 @@
 package leettests.linkedLists;
 
 /**
+ * <a href="https://leetcode.com/problems/intersection-of-two-linked-lists/">...</a>
+ *
  * Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect.
  * If the two linked lists have no intersection at all, return null.
  *
@@ -32,7 +34,6 @@ package leettests.linkedLists;
  * There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
  *
  * Example 2:
- *
  *
  * Input: intersectVal = 2, listA = [1,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
  * Output: Intersected at '2'

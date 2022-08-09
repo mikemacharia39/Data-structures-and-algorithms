0bbdeead6dc804a7536c67d36e2da4d4ad9c25ca
mikemacharia39 <mikemacharia39@gmail.com>
2022-08-09 10:55:26 +0300

    minimum number of visits

---
+++ src/codility/SmallestSubArrayAfterVisists.java
@@ -0,0 +1,22 @@
+package codility;
+
+/**
+ * Given an array where the index represents days and values are locations, assuming Dave starts the visits,
+ * when would it cost him less to do all visits
+ *
+ * Example 1: 1,7,3,2 are the locations
+ * Dave can visit {1,7,3,7,2} or {7,3,7,2,1} or {3,7,2,1}
+ * [0]=1
+ * [1]=7
+ * [2]=3
+ * [3]=7
+ * [4]=2
+ * [5]=1
+ * [6]=7
+ *
+ * {3,7,2,1} is the array when Dave was able to visit all locations at minimum cost
+ *
+ * {1,7,3,7,2,1,7}
+ */
+public class SmallestSubArrayAfterVisists {
+}

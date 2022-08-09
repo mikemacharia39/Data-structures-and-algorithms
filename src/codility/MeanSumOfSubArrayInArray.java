d4c4b662f61ee072cfabb36927869684e0972272
mikemacharia39 <mikemacharia39@gmail.com>
2022-08-09 09:49:02 +0300

    mean sum sub array

---
+++ src/codility/MeanSumOfSubArrayInArray.java
@@ -0,0 +1,17 @@
+package codility;
+
+/**
+ * Given an array nums and avg = 2, find the number of subarrays whose sum of elements divided by the number of
+ * elements is equals to the average
+ *
+ * Example:
+ *
+ * [0,4,3,-1]  avg=2
+ * e.g. [0,4] and [4,3,-1] = there are 2 contiguous subarray
+ *
+ *
+ */
+public class MeanSumOfSubArrayInArray {
+
+
+}

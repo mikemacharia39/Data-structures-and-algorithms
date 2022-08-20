0bbe687319b389a82ce13b2be1db3f425a93402f
mikemacharia39 <mikemacharia39@gmail.com>
2022-08-20 15:25:53 +0300

    Duplicate zeros question

--- src/javasolutionsforalgorithms/leettests/arrays/DuplicateZeros.java
+++ src/javasolutionsforalgorithms/leettests/arrays/DuplicateZeros.java
@@ -1,4 +1,12 @@
 package javasolutionsforalgorithms.leettests.arrays;
 
+/**
+ * Given a fixed-length integer array arr, duplicate each occurrence of zero,
+ * shifting the remaining elements to the right.
+ *
+ * Note that elements beyond the length of the original array are not written.
+ * Do the above modifications to the input array in place and do not return anything.
+ */
 public class DuplicateZeros {
+
 }

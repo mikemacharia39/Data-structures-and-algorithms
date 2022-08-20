762383ec4ef91e714379fd52bb433e4938604046
mikemacharia39 <mikemacharia39@gmail.com>
2022-08-20 15:26:33 +0300

    Duplicate zeros question, define methods

--- src/javasolutionsforalgorithms/leettests/arrays/DuplicateZeros.java
+++ src/javasolutionsforalgorithms/leettests/arrays/DuplicateZeros.java
@@ -6,7 +6,28 @@ package javasolutionsforalgorithms.leettests.arrays;
  *
  * Note that elements beyond the length of the original array are not written.
  * Do the above modifications to the input array in place and do not return anything.
+ *
+ * Example 1:
+ *
+ * Input: arr = [1,0,2,3,0,4,5,0]
+ * Output: [1,0,0,2,3,0,0,4]
+ * Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+ *
+ *
+ * Example 2:
+ *
+ * Input: arr = [1,2,3]
+ * Output: [1,2,3]
+ * Explanation: After calling your function, the input array is modified to: [1,2,3]
+ *
  */
 public class DuplicateZeros {
 
+    public void duplicateZeros(int[] arr) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }

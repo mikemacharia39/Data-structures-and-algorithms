add25db9b151f8526fa0c653f3bae7885f86b09f
mikemacharia39 <mikemacharia39@gmail.com>
2022-08-24 20:31:17 +0300

    diagonal difference

---
+++ src/javasolutionsforalgorithms/hackerrank/arrays/DiagonalDifference.java
@@ -0,0 +1,40 @@
+package javasolutionsforalgorithms.hackerrank.arrays;
+
+/**
+ * <a href="https://www.hackerrank.com/challenges/one-week-preparation-kit-diagonal-difference/problem">...</a>
+ *
+ * Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+ *
+ * For example, the square matrix  is shown below:
+ *
+ * 1 2 3
+ * 4 5 6
+ * 9 8 9
+ * The left-to-right diagonal 1 + 5 + 9 = 15. The right to left diagonal 3 + 5 + 9= 17. Their absolute difference is 2.
+ *
+ * Function description
+ *
+ * Complete the  function in the editor below.
+ *
+ * diagonal Difference takes the following parameter:
+ *
+ * int arr[n][m]: an array of integers
+ * Return
+ *
+ * int: the absolute diagonal difference
+ *
+ * Input Format
+ *
+ *
+ * Sample Input
+ *
+ * 11  2  4
+ * 4   5  6
+ * 10  8 -12
+ *
+ * Output = 15
+ *
+ */
+public class DiagonalDifference {
+
+}

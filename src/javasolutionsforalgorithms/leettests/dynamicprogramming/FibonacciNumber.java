0f6233b06e1c6abaf1112c9c18d4bab2dd04aa13
mikemacharia39 <mikemacharia39@gmail.com>
2022-09-04 21:42:08 +0300

    fibonacci number algorithm, solution 1

--- src/javasolutionsforalgorithms/leettests/dynamicprogramming/FibonacciNumber.java
+++ src/javasolutionsforalgorithms/leettests/dynamicprogramming/FibonacciNumber.java
@@ -25,4 +25,20 @@ package javasolutionsforalgorithms.leettests.dynamicprogramming;
  *
  */
 public class FibonacciNumber {
+    public int fib1(int n) {
+        if (n < 2) {
+            return n;
+        }
+
+        int num1 = 0;
+        int num2 = 1;
+        for (int i = 2; i < n; i++) {
+            int temp = num2;
+
+            num2 = num2+ num1;
+            num1 = temp;
+        }
+
+        return (num1+num2);
+    }
 }

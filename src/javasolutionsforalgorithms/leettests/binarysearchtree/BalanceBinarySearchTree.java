8f47014a4f50eabbbe449462f048c0727a09bc8c
mikemacharia39 <mikemacharia39@gmail.com>
2022-09-11 21:32:37 +0300

    Balance a binary search tree, treenode class definition

--- src/javasolutionsforalgorithms/leettests/binarysearchtree/BalanceBinarySearchTree.java
+++ src/javasolutionsforalgorithms/leettests/binarysearchtree/BalanceBinarySearchTree.java
@@ -24,4 +24,25 @@ package javasolutionsforalgorithms.leettests.binarysearchtree;
  *
  */
 public class BalanceBinarySearchTree {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode balanceBST(TreeNode root) {
+
+    }
+
 }

5ff13bf9227d4304f15ee851af327584be908164
mikemacharia39 <mikemacharia39@gmail.com>
2022-09-18 11:07:48 +0300

    Pascals triange, solution

--- src/javasolutionsforalgorithms/leettests/generic/PascalsTriangleII.java
+++ src/javasolutionsforalgorithms/leettests/generic/PascalsTriangleII.java
@@ -1,5 +1,8 @@
 package javasolutionsforalgorithms.leettests.generic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * <a href="https://leetcode.com/explore/learn/card/recursion-i/251/scenario-i-recurrence-relation/3234/">...</a>
  *
@@ -28,4 +31,28 @@ package javasolutionsforalgorithms.leettests.generic;
  * Follow up: Could you optimize your algorithm to use only O(rowIndex) extra space?
  */
 public class PascalsTriangleII {
+
+    public List<Integer> getRow(int rowIndex) {
+        // 1,1
+        // 1,2,1
+        // 1,3,3,1
+        // 1,4,6,4,1
+
+        // Initialize the list at position 0 with 1
+        // always append 1 at the end of the loop
+        // the sum = j-1+j i.e. prev pos + cur pos
+
+        List<Integer> ans = new ArrayList<>();
+        ans.add(1); // initialize position 0 to start with 1
+        for (int i = 1; i <= rowIndex; i++) {  // rowIndex is <= because at every position it is the pos+1
+            for (int j = i-1; j >= 1; j--) {
+                ans.set(j, ans.get(j) + ans.get(j-1)); // add values in reverse, this is important as you don't lose the initial/parent values
+            }
+
+            ans.add(1); // append 1 at the end after each loop
+        }
+
+
+        return ans;
+    }
 }

d2747c9072499e7200e576fdb71dc7803112c5ca
mikemacharia39 <mikemacharia39@gmail.com>
2022-09-19 22:01:41 +0300

    Grid Challenge, link to question

--- src/javasolutionsforalgorithms/hackerrank/general/GridChallenge.java
+++ src/javasolutionsforalgorithms/hackerrank/general/GridChallenge.java
@@ -5,6 +5,9 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
+ *
+ * <a href="https://www.hackerrank.com/challenges/grid-challenge/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=7-day-campaign">...</a>
+ *
  * Given a square grid of characters in the range ascii[a-z], rearrange elements of each row alphabetically, ascending.
  * Determine if the columns are also in ascending alphabetical order, top to bottom.
  * Return YES if they are or NO if they are not.

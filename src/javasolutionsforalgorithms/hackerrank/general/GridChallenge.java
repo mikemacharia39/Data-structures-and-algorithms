58b465272defa7bff6fc7901683d7c7891b2c3eb
mikemacharia39 <mikemacharia39@gmail.com>
2022-09-19 21:57:39 +0300

    Grid Challenge

---
+++ src/javasolutionsforalgorithms/hackerrank/general/GridChallenge.java
@@ -0,0 +1,35 @@
+package javasolutionsforalgorithms.hackerrank.general;
+
+/**
+ * Given a square grid of characters in the range ascii[a-z], rearrange elements of each row alphabetically, ascending.
+ * Determine if the columns are also in ascending alphabetical order, top to bottom.
+ * Return YES if they are or NO if they are not.
+ *
+ * Example 1
+ *
+ * The grid is illustrated below. grid = ["abc", "ade", "efg"]
+ *
+ * a b c
+ * a d e
+ * e f g
+ *
+ * The rows are already in alphabetical order. The columns a a e, b d f and c e g are also in alphabetical order,
+ * so the answer would be YES. Only elements within the same row can be rearranged.
+ * They cannot be moved to a different row.
+ *
+ * Function Description
+ *
+ * Complete the gridChallenge function in the editor below.
+ *
+ * gridChallenge has the following parameter(s):
+ *
+ * string grid[n]: an array of strings
+ * Returns
+ *
+ * string: either YES or NO
+ *
+ */
+public class GridChallenge {
+
+
+}

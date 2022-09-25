30f59300a8c5deec4aee8f5640c357d3189653c7
mikemacharia39 <mikemacharia39@gmail.com>
2022-09-25 19:38:46 +0300

    description and link

--- src/javasolutionsforalgorithms/leettests/dynamicprogramming/ClimbStairs2Steps.java
+++ src/javasolutionsforalgorithms/leettests/dynamicprogramming/ClimbStairs2Steps.java
@@ -1,7 +1,10 @@
 package javasolutionsforalgorithms.leettests.dynamicprogramming;
 
 /**
+ * <a href="https://leetcode.com/problems/climbing-stairs/">...</a>
  *
+ * You are climbing a staircase. It takes n steps to reach the top.
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
  */
 public class ClimbStairs2Steps {
 }

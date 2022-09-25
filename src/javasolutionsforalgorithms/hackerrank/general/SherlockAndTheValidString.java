a7072bf7d7aecc3fc270cf78c9fdf0717cce8988
mikemacharia39 <mikemacharia39@gmail.com>
2022-09-26 00:49:06 +0300

    Sherlock and valid string, link

--- src/javasolutionsforalgorithms/hackerrank/general/SherlockAndTheValidString.java
+++ src/javasolutionsforalgorithms/hackerrank/general/SherlockAndTheValidString.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
 import java.util.List;
 
 /**
+ * <a href="https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem">...</a>
+ *
  * Sherlock considers a string to be valid if all characters of the string appear the same number of times.
  * It is also valid if he can remove just 1 character at 1 index in the string,
  * and the remaining characters will occur the same number of times. Given a string ,

1a879f25de1e95d500d57b8af9c3febdc06bf4ab
mikemacharia39 <mikemacharia39@gmail.com>
2022-09-25 19:49:59 +0300

    Java stringtokens description

--- src/javasolutionsforalgorithms/hackerrank/string/JavaStringTokens.java
+++ src/javasolutionsforalgorithms/hackerrank/string/JavaStringTokens.java
@@ -1,6 +1,9 @@
 package javasolutionsforalgorithms.hackerrank.string;
 
 /**
+ *
+ * <a href="https://www.hackerrank.com/challenges/java-string-tokens/problem">...</a>
+ *
  * Given a string, , matching the regular expression [A-Za-z !,?._'@]+, split the string into tokens.
  * We define a token to be one or more consecutive English alphabetic letters.
  * Then, print the number of tokens, followed by each token on a new line.
@@ -11,6 +14,12 @@ package javasolutionsforalgorithms.hackerrank.string;
  *
  * A single string, s.
  *
+ * is composed of any of the following: English alphabetic letters, blank spaces, exclamation points (!), commas (,),
+ * question marks (?), periods (.), underscores (_), apostrophes ('), and at symbols (@).
+ * Output Format
+ *
+ * On the first line, print an integer,n , denoting the number of tokens in string s (they do not need to be unique).
+ * Next, print each of the n tokens on a new line in the same order as they appear in input string s.
  */
 public class JavaStringTokens {
 }

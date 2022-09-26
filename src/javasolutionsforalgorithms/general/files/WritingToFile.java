e99862e09cc259c96505d8b33e999cd53f095dc7
mikemacharia39 <mikemacharia39@gmail.com>
2022-09-26 23:01:03 +0300

    test write to file

--- src/javasolutionsforalgorithms/general/files/WritingToFile.java
+++ src/javasolutionsforalgorithms/general/files/WritingToFile.java
@@ -17,10 +17,15 @@ public class WritingToFile {
             System.out.println("File already exists");
         }
 
-        String sampleContent = "This is in effort ot write to file \n P.S. Let us see";
+        String sampleContent = "This is in effort ot write to file \nP.S. Let us see";
 
         FileWriter fileWriter = new FileWriter(newFileName);
         fileWriter.write(sampleContent);
         fileWriter.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        WritingToFile writingToFile = new WritingToFile();
+        writingToFile.writeInFile();
     }
 }

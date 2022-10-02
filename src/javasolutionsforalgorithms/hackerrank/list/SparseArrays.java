d6425d081246de4642f7348f9ced270d8c253569
mikemacharia39 <mikemacharia39@gmail.com>
2022-10-02 17:10:19 +0300

    Sparse Arrays: example

--- src/javasolutionsforalgorithms/hackerrank/list/SparseArrays.java
+++ src/javasolutionsforalgorithms/hackerrank/list/SparseArrays.java
@@ -6,6 +6,20 @@ package javasolutionsforalgorithms.hackerrank.list;
  * Return an array of the results.
  *
  * Example
+ *
+ * StringList = {"ab", "ab", "abc"}
+ * queries = {"ab", "abc", "bc"}
+ * There are two instances of ab, there is 1 instance of abc, there is 0 of bc, thereby the result is [2,1,0]
+ *
+ * Function Description
+ *
+ * Complete the function matchingStrings in the editor below.
+ * The function must return an array of integers representing the frequency of occurrence of each query string in stringList.
+ *
+ * matchingStrings has the following parameters:
+ *
+ * string stringList[n] - an array of strings to search
+ * string queries[q] - an array of query strings
  */
 public class SparseArrays {
 }

0bf5b2e41acae47db0beb00721e11a6cbed7e33d
mikemacharia39 <mikemacharia39@gmail.com>
2022-10-02 17:19:45 +0300

    Sparse Arrays: link to question

--- src/javasolutionsforalgorithms/hackerrank/list/SparseArrays.java
+++ src/javasolutionsforalgorithms/hackerrank/list/SparseArrays.java
@@ -6,6 +6,8 @@ import java.util.Iterator;
 import java.util.List;
 
 /**
+ * <a href="https://www.hackerrank.com/challenges/sparse-arrays/problem">...</a>
+ *
  * There is a collection of input strings and a collection of query strings.
  * For each query string, determine how many times it occurs in the list of input strings.
  * Return an array of the results.

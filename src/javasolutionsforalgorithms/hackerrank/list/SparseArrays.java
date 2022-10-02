763f985050f6135fa9d5868b38c2144aa77a1e24
mikemacharia39 <mikemacharia39@gmail.com>
2022-10-02 17:06:00 +0300

    Sparse Arrays: description of problem

---
+++ src/javasolutionsforalgorithms/hackerrank/list/SparseArrays.java
@@ -0,0 +1,11 @@
+package javasolutionsforalgorithms.hackerrank.list;
+
+/**
+ * There is a collection of input strings and a collection of query strings.
+ * For each query string, determine how many times it occurs in the list of input strings.
+ * Return an array of the results.
+ *
+ * Example
+ */
+public class SparseArrays {
+}

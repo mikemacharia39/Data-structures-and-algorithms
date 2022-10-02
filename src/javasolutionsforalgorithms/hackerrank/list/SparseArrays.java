2f807b79cc8b7602c5b9d1101470aad5bd605ad6
mikemacharia39 <mikemacharia39@gmail.com>
2022-10-02 17:15:55 +0300

    Sparse Arrays: solution 1

--- src/javasolutionsforalgorithms/hackerrank/list/SparseArrays.java
+++ src/javasolutionsforalgorithms/hackerrank/list/SparseArrays.java
@@ -1,5 +1,10 @@
 package javasolutionsforalgorithms.hackerrank.list;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * There is a collection of input strings and a collection of query strings.
  * For each query string, determine how many times it occurs in the list of input strings.
@@ -22,4 +27,35 @@ package javasolutionsforalgorithms.hackerrank.list;
  * string queries[q] - an array of query strings
  */
 public class SparseArrays {
+
+
+
+
+    /**
+     * use two loops, the top loop traverses through each query while checking its existence in the string list
+     * time complexity: O(m * n)
+     * space complexity: O(1) the list space is expected
+     * @param stringList string list
+     * @param queries queries
+     * @return list
+     */
+    public static List<Integer> matchingStrings2(List<String> stringList, List<String> queries) {
+
+        List<Integer> result = new ArrayList<>();
+        Iterator<String> iterator = queries.iterator();
+
+        while(iterator.hasNext()) {
+            String query = iterator.next();
+            int count = 0;
+            for (int i = 0; i < stringList.size(); i++) {
+                if (query.equals(stringList.get(i))) {
+                    count++;
+                }
+            }
+            result.add(count);
+            count = 0;
+        }
+
+        return result;
+    }
 }

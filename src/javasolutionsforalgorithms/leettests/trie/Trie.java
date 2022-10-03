5552aa28b586177f75194ea2fb2394a9246bf9a3
mikemacharia39 <mikemacharia39@gmail.com>
2022-10-03 23:27:41 +0300

    Trie data structure, example of trie

--- src/javasolutionsforalgorithms/leettests/trie/Trie.java
+++ src/javasolutionsforalgorithms/leettests/trie/Trie.java
@@ -1,5 +1,9 @@
 package javasolutionsforalgorithms.leettests.trie;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * A trie, also called digital tree or prefix tree, is a type of k-ary search tree,
  * a tree data structure used for locating specific keys from within a set.
@@ -20,9 +24,12 @@ package javasolutionsforalgorithms.leettests.trie;
  * <p>
  * The complexity of creating a trie is O(W*L) , where W is the number of words, and L is an average length of the word:
  * you need to perform L lookups on the average for each of the W words in the set.
- *
+ * <p>
  * Links to tutorials
  * <a href="https://www.youtube.com/watch?v=-urNrIAQnNo&ab_channel=LukasVyhnalek">...</a>
+ * https://www.youtube.com/watch?v=p4kfbNJzftQ&ab_channel=GauravSen
+ *
+ *
  * <a href="https://www.youtube.com/playlist?list=PL6Zs6LgrJj3uwRyATdtSua12k9EFQIW50">...</a>
  */
 public class Trie {
@@ -37,10 +44,17 @@ public class Trie {
         TrieNode[] children;
         boolean isWord;
 
+        int count;
+
         TrieNode() {
             this.children = new TrieNode[26];     // size 26 for only english words a -> z
             this.isWord = false;                  // if is a complete word then is true
+            this.count = 0;                       // number of string terminating at a specific node
         }
+    }
+
+    public void insertWords(List<String> words) {
+        words.forEach(this::insert);
     }
 
     public void insert(String word) {
@@ -53,18 +67,84 @@ public class Trie {
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
             int index = c - 'a';
-            TrieNode node = current.children[index];
             if (current.children[index] == null) {// check if character exists at that position
+                TrieNode node = new TrieNode();
                 current.children[index] = node;   // assign new node to pointer at the index
                 current = node;                   // point current node to node we just created
             } else {
                 current = current.children[index];// point current node to our current position
             }
+            current.count += 1;                   // increment count of node terminating on this node
         }
         current.isWord = true;
     }
 
+
+    public boolean search(String word) {
+        TrieNode currentNode = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            int index = c - 'a';
+            TrieNode node = currentNode.children[index];
+            if (node == null) {
+                return false;
+            }
+            currentNode = node;                 // point current node to our current position
+        }
+
+        return currentNode.isWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode currentNode = root;
+
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            int index = c - 'a';
+
+            TrieNode node = currentNode.children[index];
+            if (node == null) {
+                return false;
+            }
+
+            currentNode = node;
+        }
+
+        return true;
+    }
+
+    public int countPrefixStartingWith(String prefix) {
+        TrieNode currentNode = root;
+
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            int index = c - 'a';
+
+            TrieNode node = currentNode.children[index];
+            if (node == null) {
+                return 0;
+            }
+
+            currentNode = node;
+        }
+
+        return currentNode.count;
+    }
+
+
     public static void main(String[] args) {
+        List<String> dictionary = Arrays.asList("app", "apple", "applications", "cats", "cap", "cup", "ape", "hut");
+
+        Trie trie = new Trie();
+        trie.insertWords(dictionary);
+
+        List<String> searches = Arrays.asList("app", "ap", "hum", "cat", "cap", "hut");
+        searches.forEach(search -> System.out.println("Search for " + search + " | Outcome: " + trie.search(search)));
+
+        List<String> searchCounts = Arrays.asList("ap", "huge", "cat", "cap", "h");
+        searchCounts.forEach(search -> System.out.println("Search count of " + search +
+                " | Outcome: " + trie.countPrefixStartingWith(search)));
 
     }
 }

42d86af5232ef8f0354fd531af1ee34de620af47
mikemacharia39 <mikemacharia39@gmail.com>
2022-10-03 11:49:01 +0300

    Rotate Array, examples

--- src/javasolutionsforalgorithms/leettests/arrays/RotateArray.java
+++ src/javasolutionsforalgorithms/leettests/arrays/RotateArray.java
@@ -13,6 +13,27 @@ package javasolutionsforalgorithms.leettests.arrays;
  * rotate 1 steps to the right: [7,1,2,3,4,5,6]
  * rotate 2 steps to the right: [6,7,1,2,3,4,5]
  * rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ *
+ *
+ * Example 2:
+ * Input: nums = [-1,-100,3,99], k = 2
+ * Output: [3,99,-1,-100]
+ * Explanation:
+ * rotate 1 steps to the right: [99,-1,-100,3]
+ * rotate 2 steps to the right: [3,99,-1,-100]
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 105
+ * -231 <= nums[i] <= 231 - 1
+ * 0 <= k <= 105
+ *
+ *
+ * Follow up:
+ *
+ * Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
+ * Could you do it in-place with O(1) extra space?
  */
 public class RotateArray {
 }

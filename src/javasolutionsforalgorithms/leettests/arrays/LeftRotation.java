0bf03885656995dd4b42e319f24e8fe5f4ee47ee
mikemacharia39 <mikemacharia39@gmail.com>
2022-10-04 20:39:44 +0300

    Left rotation

---
+++ src/javasolutionsforalgorithms/leettests/arrays/LeftRotation.java
@@ -0,0 +1,22 @@
+package javasolutionsforalgorithms.leettests.arrays;
+
+/**
+ * A left rotation operation on an array of size n shifts each of the array's elements 1 unit to the left.
+ * Given an integer, d, rotate the array that many steps left and return the result.
+ *
+ * Example
+ * d = 2
+ * arr = [1,2,3,4,5]
+ * After 2 rotations, arr' = [3,4,5,1,2]
+ *
+ * Function Description
+ *
+ * Complete the rotateLeft function in the editor below.
+ *
+ * rotateLeft has the following parameters:
+ *
+ * int d: the amount to rotate by
+ * int arr[n]: the array to rotate
+ */
+public class LeftRotation {
+}

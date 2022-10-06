6b32f667ef4f728071e4e3897b0a34c2c4038ed6
mikemacharia39 <mikemacharia39@gmail.com>
2022-10-06 22:02:07 +0300

    Maximum element, solution

--- src/javasolutionsforalgorithms/hackerrank/stack/MaximumElement.java
+++ src/javasolutionsforalgorithms/hackerrank/stack/MaximumElement.java
@@ -1,5 +1,10 @@
 package javasolutionsforalgorithms.hackerrank.stack;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * <a href="https://www.hackerrank.com/challenges/maximum-element/problem">...</a>
  *
@@ -19,4 +24,51 @@ package javasolutionsforalgorithms.hackerrank.stack;
  * - int[]: the answers to each type 3 query
  */
 public class MaximumElement {
+    public static List<Integer> getMax(List<String> operations) {
+
+        Iterator<String> iterator = operations.iterator();
+        List<Integer> list = new ArrayList<>();
+
+        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> maxStackVal = new Stack<>();
+
+        while (iterator.hasNext()) {
+            String currentString = iterator.next();
+            char operation = currentString.charAt(0);
+            switch (operation) {
+                case '1':
+                    String[] stringPlusOp = currentString.split(" ");
+                    int element = Integer.parseInt(stringPlusOp[1]);
+                    stack.push(element);
+
+                    if (maxStackVal.isEmpty()) {
+                        maxStackVal.push(element);
+                    } else {
+                        if (element >= maxStackVal.peek()) {
+                            maxStackVal.push(element);
+                        }
+                    }
+
+                    break;
+                case '2':
+                    if (!stack.isEmpty()) {
+                        int deleted = stack.pop();
+                        int maxValPop = maxStackVal.peek();
+                        if (deleted == maxValPop) {
+                            maxStackVal.pop();
+                        }
+                    }
+                    break;
+
+                case '3':
+                    list.add(maxStackVal.peek());
+                    break;
+                default:
+                    break;
+            }
+
+        }
+        return list;
+    }
+
 }

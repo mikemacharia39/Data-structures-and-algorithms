dfa848985dea4d16f1282425e5d70e2c8a95f0cb
mikemacharia39 <mikemacharia39@gmail.com>
2022-10-06 21:59:31 +0300

    Maximum element

--- src/javasolutionsforalgorithms/hackerrank/stack/MaximumElement.java
+++ src/javasolutionsforalgorithms/hackerrank/stack/MaximumElement.java
@@ -1,7 +1,20 @@
 package javasolutionsforalgorithms.hackerrank.stack;
 
 /**
+ * You have an empty sequence, and you will be given N queries. Each query is one of these three types:
  *
+ * 1 x  -Push the element x into the stack.
+ * 2    -Delete the element present at the top of the stack.
+ * 3    -Print the maximum element in the stack.
+ * Function Description
+ *
+ * Complete the getMax function in the editor below.
+ *
+ * getMax has the following parameters:
+ * - string operations[n]: operations as strings
+ *
+ * Returns
+ * - int[]: the answers to each type 3 query
  */
 public class MaximumElement {
 }

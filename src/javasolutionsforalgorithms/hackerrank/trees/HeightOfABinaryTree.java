d6d91280a9ba81e94b07f29e3012f6be370285ed
mikemacharia39 <mikemacharia39@gmail.com>
2022-10-08 15:23:29 +0300

    Height of a binary tree, link

--- src/javasolutionsforalgorithms/hackerrank/trees/HeightOfABinaryTree.java
+++ src/javasolutionsforalgorithms/hackerrank/trees/HeightOfABinaryTree.java
@@ -1,6 +1,8 @@
 package javasolutionsforalgorithms.hackerrank.trees;
 
 /**
+ * <a href="https://www.hackerrank.com/challenges/tree-height-of-a-binary-tree/problem">...</a>
+ *
  * The height of a binary tree is the number of edges between the tree's root and its furthest leaf.
  * For example, the following binary tree is of height 2:
  */

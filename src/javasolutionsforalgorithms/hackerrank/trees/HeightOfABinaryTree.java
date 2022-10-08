ee0bfc15a4c8c4a9279d79a0100a646a8d2871b8
mikemacharia39 <mikemacharia39@gmail.com>
2022-10-08 15:27:50 +0300

    Height of a binary tree, description

--- src/javasolutionsforalgorithms/hackerrank/trees/HeightOfABinaryTree.java
+++ src/javasolutionsforalgorithms/hackerrank/trees/HeightOfABinaryTree.java
@@ -1,10 +1,61 @@
 package javasolutionsforalgorithms.hackerrank.trees;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * <a href="https://www.hackerrank.com/challenges/tree-height-of-a-binary-tree/problem">...</a>
  *
  * The height of a binary tree is the number of edges between the tree's root and its furthest leaf.
  * For example, the following binary tree is of height 2:
+ *                  4
+ *                /   \
+ *               2     6
+ *             / \    / \
+ *            1   3   5  7
+ *
+ * Function Description
+ * ----------------------
+ *
+ * Complete the getHeight or height function in the editor. It must return the height of a binary tree as an integer.
+ *
+ * getHeight or height has the following parameter(s):
+ *
+ * root: a reference to the root of a binary tree.
+ *
+ * Note -The Height of binary tree with single node is taken as zero.
+ *
  */
 public class HeightOfABinaryTree {
+    static class Node {
+        Node right;
+        Node left;
+        int val;
+
+        Node(int val) {
+            this.val = val;
+        }
+    }
+
+    public static int height(Node root) {
+        Queue<Node> queue = new LinkedList<>();
+        int counter = -1;
+        queue.add(root);
+
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            while (size != 0) {
+                Node node = queue.poll();
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                size--;
+            }
+            ++counter;
+        }
+        return counter;
+    }
 }

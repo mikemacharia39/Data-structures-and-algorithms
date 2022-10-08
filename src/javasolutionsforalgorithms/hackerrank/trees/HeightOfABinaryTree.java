57cc7b4db27b74af1f87fc5dc20d2a85c23c57b6
mikemacharia39 <mikemacharia39@gmail.com>
2022-10-08 15:46:27 +0300

    Height of a binary tree, inserting into a tree

--- src/javasolutionsforalgorithms/hackerrank/trees/HeightOfABinaryTree.java
+++ src/javasolutionsforalgorithms/hackerrank/trees/HeightOfABinaryTree.java
@@ -83,4 +83,19 @@ public class HeightOfABinaryTree {
 
         return Math.max(left, right)+1;
     }
+
+    public Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        }
+        Node cur;
+        if (data <= root.val) {
+            cur = insert(root.left, data);
+            root.left = cur;
+        } else {
+            cur = insert(root.right, data);
+            root.right = cur;
+        }
+        return root;
+    }
 }

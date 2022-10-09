3984c72a08fb1a39845f05fd7f77a897699f9167
mikemacharia39 <mikemacharia39@gmail.com>
2022-10-09 19:48:05 +0300

    SubArraySumEquals K, description

--- src/javasolutionsforalgorithms/leettests/arrays/SubArraySumEqualsK.java
+++ src/javasolutionsforalgorithms/leettests/arrays/SubArraySumEqualsK.java
@@ -1,4 +1,9 @@
 package javasolutionsforalgorithms.leettests.arrays;
 
+/**
+ * Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+ *
+ * A subarray is a contiguous non-empty sequence of elements within an array.
+ */
 public class SubArraySumEqualsK {
 }

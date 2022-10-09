06024b46aed47f090739f267be6cfc77735de42c
mikemacharia39 <mikemacharia39@gmail.com>
2022-10-09 16:06:38 +0300

    Pascals triangle, link

--- src/javasolutionsforalgorithms/leettests/generic/PascalsTriangle.java
+++ src/javasolutionsforalgorithms/leettests/generic/PascalsTriangle.java
@@ -1,6 +1,7 @@
 package javasolutionsforalgorithms.leettests.generic;
 
 /**
+ * <a href="https://leetcode.com/problems/pascals-triangle/">...</a>
  *
  * Given an integer numRows, return the first numRows of Pascal's triangle
  * In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:

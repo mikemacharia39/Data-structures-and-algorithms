688a10f3dd2a3074557f9facc6a617d963188938
mikemacharia39 <mikemacharia39@gmail.com>
2022-10-20 20:28:51 +0300

    TwoSumIVInputIsABST, implementation using stack

--- src/javasolutionsforalgorithms/leettests/binarysearchtree/TwoSumIVInputIsABST.java
+++ src/javasolutionsforalgorithms/leettests/binarysearchtree/TwoSumIVInputIsABST.java
@@ -1,5 +1,9 @@
 package javasolutionsforalgorithms.leettests.binarysearchtree;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
 /**
  * Given the root of a Binary Search Tree and a target number k,
  * return true if there exist two elements in the BST such that their sum is equal to the given target.
@@ -25,4 +29,24 @@ public class TwoSumIVInputIsABST {
     }
 
 
+    public boolean findTarget_usingStack(TreeNode root, int k) {
+        Set<Integer> set = new HashSet<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+
+            if (set.contains(k - root.val)) {
+                return true;
+            } else {
+                set.add(root.val);
+            }
+            root = root.right;
+        }
+
+        return false;
+    }
 }

d7b0ce8e262cfe1a2a4bd8ff7e50f7ae47c92a8c
mikemacharia39 <mikemacharia39@gmail.com>
2022-10-20 19:27:57 +0300

    Pangram, solution 2

--- src/javasolutionsforalgorithms/leettests/interestingquestions/CheckIfTheSentenceIsPangram.java
+++ src/javasolutionsforalgorithms/leettests/interestingquestions/CheckIfTheSentenceIsPangram.java
@@ -1,5 +1,8 @@
 package javasolutionsforalgorithms.leettests.interestingquestions;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * A pangram is a sentence where every letter of the English alphabet appears at least once.
  *
@@ -20,6 +23,21 @@ package javasolutionsforalgorithms.leettests.interestingquestions;
  * Output: false
  */
 public class CheckIfTheSentenceIsPangram {
+
+
+
+    /**
+     * Solution 2
+     * @param sentence sentence
+     * @return true|false
+     */
+    public boolean checkIfPangram1(String sentence) {
+        Set<Character> set = new HashSet<>();
+        for(int i = 0; i < sentence.length(); i++) {
+            set.add(sentence.charAt(i));
+        }
+        return set.size() == 26;
+    }
 
     public boolean checkIfPangram2(String sentence) {
         int[] alphabetFeq = new int[26];

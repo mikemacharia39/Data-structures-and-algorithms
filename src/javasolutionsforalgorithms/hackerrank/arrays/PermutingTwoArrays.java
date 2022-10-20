441945453f14c0c108ded7f53f7772f2e9b0cd6e
mikemacharia39 <mikemacharia39@gmail.com>
2022-10-20 18:16:06 +0300

    Permuting two arrays, solution

--- src/javasolutionsforalgorithms/hackerrank/arrays/PermutingTwoArrays.java
+++ src/javasolutionsforalgorithms/hackerrank/arrays/PermutingTwoArrays.java
@@ -1,9 +1,26 @@
 package javasolutionsforalgorithms.hackerrank.arrays;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * https://www.hackerrank.com/challenges/two-arrays/problem
  *
  */
 public class PermutingTwoArrays {
 
+    public static String twoArrays(int k, List<Integer> A, List<Integer> B) {
+
+        Collections.sort(A);
+        B.sort(Comparator.reverseOrder());
+
+        for (int i = 0; i < A.size(); i++) {
+            if ((A.get(i) + B.get(i)) < k) {
+                return "NO";
+            }
+        }
+
+        return "YES";
+    }
 }

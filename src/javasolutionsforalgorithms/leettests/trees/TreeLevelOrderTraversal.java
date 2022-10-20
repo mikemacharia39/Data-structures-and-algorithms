01572409b263c1bd1a89a5046f807e15d77c0f52
mikemacharia39 <mikemacharia39@gmail.com>
2022-10-20 16:36:13 +0300

    description

--- src/javasolutionsforalgorithms/leettests/trees/TreeLevelOrderTraversal.java
+++ src/javasolutionsforalgorithms/leettests/trees/TreeLevelOrderTraversal.java
@@ -1,4 +1,32 @@
 package javasolutionsforalgorithms.leettests.trees;
 
+/**
+ * Given a pointer to the root of a binary tree, you need to print the level order traversal of this tree.
+ * In level-order traversal, nodes are visited level by level from left to right.
+ * Complete the function levelOrder and print the values in a single line separated by a space.
+ *
+ * For example:
+ *
+ *      1
+ *       \
+ *        2
+ *         \
+ *          5
+ *         /  \
+ *        3    6
+ *         \
+ *          4
+ * For the above tree, the level order traversal is 1 -> 2 -> 5 -> 3 -> 6 -> 4.
+ *
+ * Input Format
+ *
+ * You are given a function,
+ *
+ * void levelOrder(Node * root) {
+ *
+ * }
+ *
+ *
+ */
 public class TreeLevelOrderTraversal {
 }

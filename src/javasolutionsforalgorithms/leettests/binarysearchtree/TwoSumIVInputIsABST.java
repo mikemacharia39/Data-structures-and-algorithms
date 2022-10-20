4359b589b6d4eed5da76a5b6c17b93dfbf578b37
mikemacharia39 <mikemacharia39@gmail.com>
2022-10-20 20:26:52 +0300

    TwoSumIVInputIsABST, description

--- src/javasolutionsforalgorithms/leettests/binarysearchtree/TwoSumIVInputIsABST.java
+++ src/javasolutionsforalgorithms/leettests/binarysearchtree/TwoSumIVInputIsABST.java
@@ -1,4 +1,18 @@
 package javasolutionsforalgorithms.leettests.binarysearchtree;
 
+/**
+ * Given the root of a Binary Search Tree and a target number k,
+ * return true if there exist two elements in the BST such that their sum is equal to the given target.
+ *
+ * Example 1:
+ *              5
+ *            /   \
+ *           3     6
+ *         /  \      \
+ *        2   4       7
+ * Input: root = [5,3,6,2,4,null,7], k = 9
+ * Output: true
+ */
 public class TwoSumIVInputIsABST {
+
 }

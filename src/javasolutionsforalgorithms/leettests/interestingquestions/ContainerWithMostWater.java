4b7828c7bc6093c431ef455bd7829fd519d5fc43
mikemacharia39 <mikemacharia39@gmail.com>
2022-10-20 19:12:45 +0300

    Container with most water, link

--- src/javasolutionsforalgorithms/leettests/interestingquestions/ContainerWithMostWater.java
+++ src/javasolutionsforalgorithms/leettests/interestingquestions/ContainerWithMostWater.java
@@ -1,6 +1,8 @@
 package javasolutionsforalgorithms.leettests.interestingquestions;
 
 /**
+ * <a href="https://leetcode.com/problems/container-with-most-water/">...</a>
+ *
  * You are given an integer array height of length n.
  * There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
  *

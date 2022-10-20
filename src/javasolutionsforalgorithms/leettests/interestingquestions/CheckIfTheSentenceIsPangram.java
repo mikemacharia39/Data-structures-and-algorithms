c878312c07d6bcd449d69aa6516e3acdacd9e131
mikemacharia39 <mikemacharia39@gmail.com>
2022-10-20 19:26:05 +0300

    Pangram, explanation

--- src/javasolutionsforalgorithms/leettests/interestingquestions/CheckIfTheSentenceIsPangram.java
+++ src/javasolutionsforalgorithms/leettests/interestingquestions/CheckIfTheSentenceIsPangram.java
@@ -1,4 +1,10 @@
 package javasolutionsforalgorithms.leettests.interestingquestions;
 
+/**
+ * A pangram is a sentence where every letter of the English alphabet appears at least once.
+ *
+ * Given a string sentence containing only lowercase English letters,
+ * return true if sentence is a pangram, or false otherwise.
+ */
 public class CheckIfTheSentenceIsPangram {
 }

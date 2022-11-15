26452d3139883801e912a54aceaafe2539c199fc
mikemacharia39 <mikemacharia39@gmail.com>
2022-11-15 23:05:27 +0300

    Number of island, examples

--- src/javasolutionsforalgorithms/leettests/dynamicprogramming/NumberOfIslands.java
+++ src/javasolutionsforalgorithms/leettests/dynamicprogramming/NumberOfIslands.java
@@ -5,6 +5,27 @@ package javasolutionsforalgorithms.leettests.dynamicprogramming;
  *
  * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
  * You may assume all four edges of the grid are all surrounded by water.
+ *
+ * Example 1:
+ *
+ * Input: grid = [
+ *   ["1","1","1","1","0"],
+ *   ["1","1","0","1","0"],
+ *   ["1","1","0","0","0"],
+ *   ["0","0","0","0","0"]
+ * ]
+ * Output: 1
+ *
+ * Example 2:
+ *
+ * Input: grid = [
+ *   ["1","1","0","0","0"],
+ *   ["1","1","0","0","0"],
+ *   ["0","0","1","0","0"],
+ *   ["0","0","0","1","1"]
+ * ]
+ * Output: 3
  */
 public class NumberOfIslands {
+
 }

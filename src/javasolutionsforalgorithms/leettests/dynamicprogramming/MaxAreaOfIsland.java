07cd9eb32abaaefbda042ae8a09cb12a09860549
mikemacharia39 <mikemacharia39@gmail.com>
2022-11-19 12:52:30 +0300

    Max Area Of Island, solution

--- src/javasolutionsforalgorithms/leettests/dynamicprogramming/MaxAreaOfIsland.java
+++ src/javasolutionsforalgorithms/leettests/dynamicprogramming/MaxAreaOfIsland.java
@@ -1,6 +1,8 @@
 package javasolutionsforalgorithms.leettests.dynamicprogramming;
 
 /**
+ * <a href="https://leetcode.com/problems/max-area-of-island/">...</a>
+ *
  * You are given an m x n binary matrix grid. An island is a group of 1's (representing land)
  * connected 4-directionally (horizontal or vertical.)
  * You may assume all four edges of the grid are surrounded by water.
@@ -11,4 +13,39 @@ package javasolutionsforalgorithms.leettests.dynamicprogramming;
  */
 public class MaxAreaOfIsland {
 
+    public int maxAreaOfIsland(int[][] grid) {
+        if (grid.length == 0) {
+            return 0;
+        }
+        int maxArea = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    maxArea = Math.max(maxArea, dfsFilling(grid, i, j));
+                }
+            }
+        }
+
+        return maxArea;
+    }
+
+    public int dfsFilling(int[][] grid, int row, int col) {
+        if (row < 0 || col < 0 || row > grid.length - 1 || col > grid[0].length - 1) {
+            return 0;
+        }
+        if (grid[row][col] == 0) {
+            return 0;
+        }
+        int count = 1; // having entered the loop
+
+        // mark the areas as visited
+        grid[row][col] = 0;
+
+        count += dfsFilling(grid, row+1, col);
+        count += dfsFilling(grid, row-1, col);
+        count += dfsFilling(grid, row, col+1);
+        count += dfsFilling(grid, row, col-1);
+
+        return count;
+    }
 }

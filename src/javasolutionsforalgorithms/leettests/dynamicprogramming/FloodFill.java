a1168f1ec7c464766ba19fb5c6299f14190f71a1
mikemacharia39 <mikemacharia39@gmail.com>
2022-11-30 21:28:35 +0300

    Flood fill, solution

--- src/javasolutionsforalgorithms/leettests/dynamicprogramming/FloodFill.java
+++ src/javasolutionsforalgorithms/leettests/dynamicprogramming/FloodFill.java
@@ -12,7 +12,52 @@ package javasolutionsforalgorithms.leettests.dynamicprogramming;
  * Replace the color of all the aforementioned pixels with color.
  *
  * Return the modified image after performing the flood fill.
+ *
+ * Example 1:
+ *
+ * [1,1,1]
+ * [1,1,0]
+ * [1,0,1]
+ *
+ * Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, color = 2
+ * Output: [[2,2,2],[2,2,0],[2,0,1]]
+ * Explanation: From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel), all pixels connected by a path of the same color as the starting pixel (i.e., the blue pixels) are colored with the new color.
+ * Note the bottom corner is not colored 2, because it is not 4-directionally connected to the starting pixel.
+ *
+ *
+ * Example 2:
+ *
+ * Input: image = [[0,0,0],[0,0,0]], sr = 0, sc = 0, color = 0
+ * Output: [[0,0,0],[0,0,0]]
+ * Explanation: The starting pixel is already colored 0, so no changes are made to the image.
+ *
  */
 public class FloodFill {
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        if(image[sr][sc] == color) {
+            return image;
+        }
+
+        dfsFill(image, sr, sc, image[sr][sc], color);
+
+        return image;
+    }
+
+    private void dfsFill(int[][] image, int sr, int sc, int currImage, int newColor) {
+        if (sr < 0 || sc < 0 || sr > image.length - 1 || sc > image[0].length - 1) {
+            return;
+        }
+
+        if (image[sr][sc] != currImage) {
+            return;
+        }
+
+        dfsFill(image, sr-1, sc, currImage, newColor);
+        dfsFill(image, sr+1, sc, currImage, newColor);
+        dfsFill(image, sr, sc-1, currImage, newColor);
+        dfsFill(image, sr, sc+1, currImage, newColor);
+    }
+
 
 }

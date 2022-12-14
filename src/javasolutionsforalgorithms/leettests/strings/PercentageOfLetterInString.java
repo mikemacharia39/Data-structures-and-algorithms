5c1a4fb11de95cdfba9dc282fb913d1ade039770
mikemacharia39 <mikemacharia39@gmail.com>
2022-12-14 13:44:49 +0300

    Percentage, solution 1

--- src/javasolutionsforalgorithms/leettests/strings/PercentageOfLetterInString.java
+++ src/javasolutionsforalgorithms/leettests/strings/PercentageOfLetterInString.java
@@ -21,4 +21,15 @@ package javasolutionsforalgorithms.leettests.strings;
  * The percentage of characters in s that equal the letter 'k' is 0%, so we return 0.
  */
 public class PercentageOfLetterInString {
+
+    public int percentageLetterSln1(String s, char letter) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == letter) {
+                count++;
+            }
+        }
+
+        return (count * 100)/s.length();
+    }
 }

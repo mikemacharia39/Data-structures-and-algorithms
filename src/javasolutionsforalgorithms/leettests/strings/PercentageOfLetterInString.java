e04e38e41c4cc89a2c147ab29f65d1604401e6f9
mikemacharia39 <mikemacharia39@gmail.com>
2022-12-14 13:44:13 +0300

    Percentage, link

--- src/javasolutionsforalgorithms/leettests/strings/PercentageOfLetterInString.java
+++ src/javasolutionsforalgorithms/leettests/strings/PercentageOfLetterInString.java
@@ -1,2 +1,24 @@
-package javasolutionsforalgorithms.leettests.strings;public class PercentageOfLetterInString {
+package javasolutionsforalgorithms.leettests.strings;
+
+/**
+ * <a href="https://leetcode.com/problems/percentage-of-letter-in-string/">...</a>
+ *
+ * Given a string s and a character letter, return the percentage of characters in s that equal letter rounded down to
+ * the nearest whole percent.
+ *
+ * Example 1:
+ *
+ * Input: s = "foobar", letter = "o"
+ * Output: 33
+ * Explanation:
+ * The percentage of characters in s that equal the letter 'o' is 2 / 6 * 100% = 33% when rounded down, so we return 33.
+ *
+ * Example 2:
+ *
+ * Input: s = "jjjj", letter = "k"
+ * Output: 0
+ * Explanation:
+ * The percentage of characters in s that equal the letter 'k' is 0%, so we return 0.
+ */
+public class PercentageOfLetterInString {
 }

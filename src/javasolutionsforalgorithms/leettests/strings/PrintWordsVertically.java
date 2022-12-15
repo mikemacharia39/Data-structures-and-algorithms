d760ac9062508377a29ee4ab75108c5de3997e59
mikemacharia39 <mikemacharia39@gmail.com>
2022-12-15 16:33:42 +0300

    Print words vertically, helper functions

--- src/javasolutionsforalgorithms/leettests/strings/PrintWordsVertically.java
+++ src/javasolutionsforalgorithms/leettests/strings/PrintWordsVertically.java
@@ -5,22 +5,22 @@ import java.util.List;
 
 /**
  * <a href="https://leetcode.com/problems/print-words-vertically/">...</a>
- *
+ * <p>
  * Given a string s. Return all the words vertically in the same order in which they appear in s.
  * Words are returned as a list of strings, complete with spaces when is necessary. (Trailing spaces are not allowed).
  * Each word would be put on only one column and that in one column there will be only one word.
- *
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: s = "HOW ARE YOU"
  * Output: ["HAY","ORO","WEU"]
  * Explanation: Each word is printed vertically.
- *  "HAY"
- *  "ORO"
- *  "WEU"
- *
+ * "HAY"
+ * "ORO"
+ * "WEU"
+ * <p>
  * Example 2:
- *
+ * <p>
  * Input: s = "TO BE OR NOT TO BE"
  * Output: ["TBONTB","OEROOE","   T"]
  * Explanation: Trailing spaces is not allowed.

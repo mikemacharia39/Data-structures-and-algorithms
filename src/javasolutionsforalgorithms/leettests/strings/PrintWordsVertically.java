8f9b16ed15e6f4714794c6efb9a8513b0facc1ac
mikemacharia39 <mikemacharia39@gmail.com>
2022-12-15 16:16:24 +0300

    Print words vertically, link

--- src/javasolutionsforalgorithms/leettests/strings/PrintWordsVertically.java
+++ src/javasolutionsforalgorithms/leettests/strings/PrintWordsVertically.java
@@ -1,6 +1,8 @@
 package javasolutionsforalgorithms.leettests.strings;
 
 /**
+ * <a href="https://leetcode.com/problems/print-words-vertically/">...</a>
+ *
  * Given a string s. Return all the words vertically in the same order in which they appear in s.
  * Words are returned as a list of strings, complete with spaces when is necessary. (Trailing spaces are not allowed).
  * Each word would be put on only one column and that in one column there will be only one word.

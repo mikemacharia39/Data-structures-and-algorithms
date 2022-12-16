7f7b7e6506f024f0357c6061ff2aa1150ffd4796
mikemacharia39 <mikemacharia39@gmail.com>
2022-12-16 15:30:57 +0300

    sender with largest word count, example

--- src/javasolutionsforalgorithms/leettests/arrays/SenderWithLargestWordCount.java
+++ src/javasolutionsforalgorithms/leettests/arrays/SenderWithLargestWordCount.java
@@ -1,7 +1,27 @@
 package javasolutionsforalgorithms.leettests.arrays;
 
 /**
+ * You have a chat log of n messages.
+ * You are given two string arrays messages and senders where messages[i] is a message sent by senders[i].
  *
+ * A message is list of words that are separated by a single space with no leading or trailing spaces.
+ * The word count of a sender is the total number of words sent by the sender.
+ * Note that a sender may send more than one message.
+ *
+ * Return the sender with the largest word count.
+ * If there is more than one sender with the largest word count, return the one with the lexicographically largest name.
+ *
+ * Note:
+ * Uppercase letters come before lowercase letters in lexicographical order.
+ * "Alice" and "alice" are distinct.
+ *
+ * Example 1:
+ *
+ * Input: messages = ["How is leetcode for everyone","Leetcode is useful for practice"], senders = ["Bob","Charlie"]
+ * Output: "Charlie"
+ * Explanation: Bob sends a total of 5 words.
+ * Charlie sends a total of 5 words.
+ * Since there is a tie for the largest word count, we return the sender with the lexicographically larger name, Charlie.
  */
 public class SenderWithLargestWordCount {
 }

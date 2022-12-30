565f7830f3956848a3d7beb6cdc8ce370e9cf47c
mikemacharia39 <mikemacharia39@gmail.com>
2022-12-30 14:51:27 +0300

    Min Diff in 3 moves, comment

--- src/javasolutionsforalgorithms/leettests/arrays/MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves.java
+++ src/javasolutionsforalgorithms/leettests/arrays/MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves.java
@@ -7,6 +7,17 @@ import java.util.Arrays;
  * You are given an integer array nums.
  * In one move, you can choose one element of nums and change it to any value.
  * Return the minimum difference between the largest and smallest value of nums after performing at most three moves.
+ *
+ * Example:
+ *
+ * Input: nums = [1,5,0,10,14]
+ * Output: 1
+ * Explanation: We can make at most 3 moves.
+ * In the first move, change 5 to 0. nums becomes [1,0,0,10,14].
+ * In the second move, change 10 to 0. nums becomes [1,0,0,0,14].
+ * In the third move, change 14 to 1. nums becomes [1,0,0,0,1].
+ * After performing 3 moves, the difference between the minimum and maximum is 1 - 0 = 0.
+ * It can be shown that there is no way to make the difference 0 in 3 moves.
  */
 public class MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves {
     public int minDifference(int[] nums) {
@@ -20,7 +31,7 @@ public class MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves {
         int min = Integer.MAX_VALUE;
         int n = nums.length;
         int start = 0;
-        int end = nums.length - 3 - 1; // crucial
+        int end = nums.length - 3 - 1; // crucial and caters for number of moves
         while (start < n && end < n) {
             min = Math.min(min, nums[end] - nums[start]);
             start++;

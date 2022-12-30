461dd76ff4fb618a172e03a59fb244f29d000f38
mikemacharia39 <mikemacharia39@gmail.com>
2022-12-30 14:52:00 +0300

    Min Diff in 3 moves, link

--- src/javasolutionsforalgorithms/leettests/arrays/MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves.java
+++ src/javasolutionsforalgorithms/leettests/arrays/MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves.java
@@ -4,6 +4,8 @@ package javasolutionsforalgorithms.leettests.arrays;
 import java.util.Arrays;
 
 /**
+ * <a href="https://leetcode.com/problems/minimum-difference-between-largest-and-smallest-value-in-three-moves/">...</a>
+ *
  * You are given an integer array nums.
  * In one move, you can choose one element of nums and change it to any value.
  * Return the minimum difference between the largest and smallest value of nums after performing at most three moves.

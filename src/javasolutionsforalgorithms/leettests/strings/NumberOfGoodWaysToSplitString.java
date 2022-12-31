bef7fd6da292b295bd1ef10abd9ea20ef79a9b35
mikemacharia39 <mikemacharia39@gmail.com>
2022-12-31 09:16:56 +0300

    Ways to split string, bad solution

--- src/javasolutionsforalgorithms/leettests/strings/NumberOfGoodWaysToSplitString.java
+++ src/javasolutionsforalgorithms/leettests/strings/NumberOfGoodWaysToSplitString.java
@@ -1,5 +1,8 @@
 package javasolutionsforalgorithms.leettests.strings;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * You are given a string s.
  *
@@ -11,4 +14,32 @@ package javasolutionsforalgorithms.leettests.strings;
  */
 public class NumberOfGoodWaysToSplitString {
 
+
+    /**
+     * Timed-out
+     * This is not a good solution. It times out in case when the string becomes very long, but it works ;)
+     */
+    public int numSplits2(String s) {
+        int splits = 0;
+
+        for (int i = 1; i < s.length(); i++) {
+            String left = s.substring(0,i);
+            String right = s.substring(i);
+            if (getDistinctCount(left) == getDistinctCount(right)) {
+                splits++;
+            }
+        }
+
+        return splits;
+    }
+
+
+    private int getDistinctCount(String s) {
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            set.add(s.charAt(i));
+        }
+
+        return set.size();
+    }
 }

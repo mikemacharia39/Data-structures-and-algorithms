11393160f4a6befee43a17123f490ef8bab28354
mikemacharia39 <mikemacharia39@gmail.com>
2023-01-22 22:27:35 +0300

    Non degenerate triangle, solution

--- src/javasolutionsforalgorithms/hackerrank/math/NonDegenerateTriangle.java
+++ src/javasolutionsforalgorithms/hackerrank/math/NonDegenerateTriangle.java
@@ -4,6 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Given an array of stick lengths, use 3 of them to construct a non-degenerate triangle with the maximum possible perimeter.
+ * Return an array of the lengths of its sides as 3 integers in non-decreasing order.
+ *
+ * If there are several valid triangles having the maximum perimeter:
+ *
+ * Choose the one with the longest maximum side.
+ * If more than one has that maximum, choose from them the one with the longest minimum side.
+ * If more than one has that maximum as well, print any one them.
+ * If no non-degenerate triangle exists, return [-1].
+ * 
+ *
+ * Sample Input 0
+ *
+ * 1 1 1 3 3
+ * Sample Output 0
+ *
+ * 1 3 3
+ *
+ * -------------------------------------------------------------------------------------------
+ *
  * Non-degenerate triangle − it is a triangle that has a positive area.
  * The condition for a non-degenerate triangle with sides a, b, c is −
  *

45fea99c1fe2122de99a57e8e7a2fa5ce37edf70
mikemacharia39 <mikemacharia39@gmail.com>
2023-01-22 21:21:31 +0300

    Beautiful pairs, solution

--- src/javasolutionsforalgorithms/hackerrank/list/BeautifulPairs.java
+++ src/javasolutionsforalgorithms/hackerrank/list/BeautifulPairs.java
@@ -1,4 +1,40 @@
 package javasolutionsforalgorithms.hackerrank.list;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BeautifulPairs {
+    public static int beautifulPairs(List<Integer> A, List<Integer> B) {
+        // both list A and B are of equal size
+        // create temp list to hold elements of either Array
+        List<Integer> temp = new ArrayList<>(A);
+
+        int n = temp.size();
+
+        int countDisjoint = 0;
+
+        for (Integer num : temp) {
+            if (A.contains(num) && B.contains(num)) {
+                countDisjoint++;
+
+                A.remove(num);
+                B.remove(num);
+            }
+        }
+
+        // Your task is to change exactly 1 element in B so that the size of
+        // the pairwise disjoint beautiful set is maximum.
+        if (A.size() >= 1 && B.size() >= 1) {
+            countDisjoint++;
+        } else if (n == countDisjoint) {
+            // why do we have this here?
+            // Because "Your task is to change exactly 1 element",
+
+            // if all elements matched then you change 1 element, then the
+            // countDisjoint decreases
+            countDisjoint--;
+        }
+
+        return countDisjoint;
+    }
 }

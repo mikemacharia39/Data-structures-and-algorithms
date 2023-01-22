b94db47a024d9a948b2bcc6acb2a458f47d71a71
mikemacharia39 <mikemacharia39@gmail.com>
2023-01-22 10:45:22 +0300

    count vowels in string, solutions

--- src/javasolutionsforalgorithms/leettests/strings/CountVowelSubstringsOfAString.java
+++ src/javasolutionsforalgorithms/leettests/strings/CountVowelSubstringsOfAString.java
@@ -22,6 +22,30 @@ import java.util.Set;
 public class CountVowelSubstringsOfAString {
 
     /**
+     * Pretty good solution
+     * @param word word
+     * @return number of contiguous vowels
+     */
+    public int countVowelSubstrings(String word) {
+        int vow = 0;
+        int n = word.length();
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < n - 4; i++) {
+            set.clear();
+            for (int j = i; j < n; j++) {
+                char ch = word.charAt(j);
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    set.add(ch);
+                    if (set.size() == 5)
+                        vow++;
+                } else
+                    break;
+            }
+        }
+        return vow;
+    }
+
+    /**
      * Brute force solution, for easy understanding
      */
     public int countVowelSubstringsSln1(String word) {

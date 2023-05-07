bfe018862f07bb8d38f82df646ea64dcd13a5637
mikemacharia39 <mikemacharia39@gmail.com>
2023-05-07 17:45:18 +0300

    more optimal solution

--- src/javasolutionsforalgorithms/leettests/arrays/NextGreaterElement1.java
+++ src/javasolutionsforalgorithms/leettests/arrays/NextGreaterElement1.java
@@ -1,5 +1,8 @@
 package javasolutionsforalgorithms.leettests.arrays;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 /**
  * <a href="https://leetcode.com/problems/next-greater-element-i/">...</a>
  *
@@ -20,7 +23,41 @@ package javasolutionsforalgorithms.leettests.arrays;
  */
 public class NextGreaterElement1 {
 
+    /**
+     * A more optimal solution
+     * @param nums1 - subset of nums2
+     * @param nums2 - array
+     * @return - array of next greater elements
+     */
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums2.length; i++) {
+            map.put(nums2[i], i);
+        }
+        int[] ans = new int[nums1.length];
+        Arrays.fill(ans, -1);
+
+        int len2 = nums2.length;
+        for (int i = 0; i < nums1.length; i++) {
+            if (map.containsKey(nums1[i])) {
+                int j = map.get(nums1[i]) + 1;
+                while (len2 > j) {
+                    if (nums2[j] > nums1[i]) {
+                        ans[i] = nums2[j];
+                        break;
+                    }
+
+                    j++;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    public int[] nextGreaterElement2(int[] nums1, int[] nums2) {
         int[] ans = new int[nums1.length];
 
         for(int i = 0; i < nums1.length; i++) {

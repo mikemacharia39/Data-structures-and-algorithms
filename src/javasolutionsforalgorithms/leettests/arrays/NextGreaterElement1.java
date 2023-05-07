3fa7fbdc7c0943f145a653f746cf8797d8a69676
mikemacharia39 <mikemacharia39@gmail.com>
2023-05-07 17:00:15 +0300

    function to check if next value is greater main fun

--- src/javasolutionsforalgorithms/leettests/arrays/NextGreaterElement1.java
+++ src/javasolutionsforalgorithms/leettests/arrays/NextGreaterElement1.java
@@ -2,6 +2,15 @@ package javasolutionsforalgorithms.leettests.arrays;
 
 public class NextGreaterElement1 {
 
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] ans = new int[nums1.length];
+
+        for(int i = 0; i < nums1.length; i++) {
+            ans[i] = checkIfNextValue(nums1[i], nums2);
+        }
+
+        return ans;
+    }
 
     private static int checkIfNextValue(int val, int[] nums2) {
         int count = 1;

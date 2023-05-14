0755336d38911ab6db3e6eab18e26006f1c2aa62
mikemacharia39 <mikemacharia39@gmail.com>
2023-05-14 15:47:56 +0300

    count of min and max of number until repetion stops

---
+++ src/javasolutionsforalgorithms/general/DifferenceMaxAndMinOfNoUntilOriginal.java
@@ -0,0 +1,97 @@
+package javasolutionsforalgorithms.general;
+
+/**
+ * This class is a solution to the following problem:
+ * to find the difference between the largest and smallest digits of a number.
+ * The number is limited to 4 digits.
+ */
+public class DifferenceMaxAndMinOfNoUntilOriginal {
+
+    public static int countNoOfDiffUntilOriginalNumIsObtained(int num) {
+        int count = 0;
+        int original = num;
+
+        while (findDiff(num) != original) {
+            num = findDiff(num);
+            count++;
+            System.out.println("count: " + count + ", diff: " + num);
+
+            if (count > 10 || num == 0) {
+                break;
+            }
+        }
+
+        return count;
+    }
+
+    public static int findDiff(int num) {
+        int sortNum = sortDigitOfNumInAsc(num);
+        int reverseNum = reverseDigitsOfANumber(sortNum);
+        System.out.printf("numSortedAsc: %d, numSortedDesc: %d%n",
+                sortNum, reverseNum);
+        return Math.abs(reverseNum - sortNum);
+    }
+
+    public static int sortDigitOfNumInAsc(int num) {
+        int[] digits = new int[10];
+
+        // the digits array will contain the count of each digit in the number
+        // e.g. 1234 will be stored as [0, 1, 1, 1, 1, 0, 0, 0, 0, 0]
+        while (num > 0) {
+            int modulus = num % 10;
+            digits[modulus]++;
+            num /= 10;
+        }
+
+        // now we will iterate over the digits array and create the number
+        int ans = 0;
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] > 0) {
+                while (digits[i]-- > 0) {
+                    ans = ans * 10 + i;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static int reverseDigitsOfANumber(int num) {
+        int ans = 0;
+
+        while (num > 0) {
+            int modulus = num % 10;
+            ans = ans * 10 + modulus;
+            num /= 10;
+        }
+
+        return padNumWithZerosWhenLessThan4Digits(ans);
+    }
+
+    public static int padNumWithZerosWhenLessThan4Digits(int num) {
+        int ans = 0;
+
+        if (num < 10) {
+            ans = num * 1000;
+        } else if (num < 100) {
+            ans = num * 100;
+        } else if (num < 1000) {
+            ans = num * 10;
+        } else {
+            ans = num;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int num = 6174;
+        System.out.println("num: " + num);
+        System.out.println("countNoOfDiffUntilOriginalNumIsObtained: " + countNoOfDiffUntilOriginalNumIsObtained(num));
+
+        System.out.println("====================================");
+
+        int num2 = 8;
+        System.out.println("num: " + num2);
+        System.out.println("countNoOfDiffUntilOriginalNumIsObtained: " + countNoOfDiffUntilOriginalNumIsObtained(num2));
+    }
+}

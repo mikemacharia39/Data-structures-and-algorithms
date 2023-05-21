2d1fbfdc959e6cdedee85cd15ab0fb0e7a15f305
mikemacharia39 <mikemacharia39@gmail.com>
2023-05-21 20:06:25 +0300

    Add array and number

---
+++ src/javasolutionsforalgorithms/leettests/arrays/AddToArrayFormInteger.java
@@ -0,0 +1,89 @@
+package javasolutionsforalgorithms.leettests.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddToArrayFormInteger {
+
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer> result = new ArrayList<>();
+        int len = num.length - 1;
+
+        while(len >= 0 || k != 0) {
+            if(len >= 0) {
+                k += num[len--];
+            }
+            result.add(0, k % 10);
+            k /= 10;
+        }
+
+        return result;
+    }
+    public List<Integer> addToArrayFormSolution3(int[] num, int k) {
+        int kCopy = k;
+        int digitsInK = 0;
+        while (kCopy > 0) {
+            kCopy /= 10;
+            digitsInK++;
+        }
+
+        int[] kArr = new int[digitsInK];
+        int i = digitsInK;
+        for (int j = 0; j < digitsInK; j++) {
+            kArr[--i] = k % 10;
+            k /= 10;
+        }
+
+        List<Integer> ans = new ArrayList<>();
+
+        int carry = 0;
+
+        int x = num.length - 1;
+        int y = kArr.length - 1;
+
+        while (x >= 0 || y >= 0) {
+            if (x >= 0) {
+                carry += num[x--];
+            }
+            if (y >= 0) {
+                carry += kArr[y--];
+            }
+            ans.add(0, carry % 10);
+            carry = carry / 10;
+        }
+
+        if (carry > 0) {
+            ans.add(0 , carry);
+        }
+
+        return ans;
+    }
+
+
+
+    public List<Integer> addToArrayFormSln2(int[] num, int k) {
+        int numArrToInt = 0;
+        for (int j : num) {
+            numArrToInt = numArrToInt * 10 + j;
+        }
+
+        numArrToInt += k;
+
+        List<Integer> ans = new ArrayList<>();
+        while (numArrToInt > 0) {
+            int modulus = numArrToInt % 10;
+            ans.add(0, modulus);
+
+            numArrToInt /= 10;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] num = {1, 2, 0, 0};
+        int k = 34;
+
+        AddToArrayFormInteger addToArrayFormInteger = new AddToArrayFormInteger();
+        System.out.println(addToArrayFormInteger.addToArrayForm(num, k));
+    }
+}

39081f1faf37f1214f932d670b036fe5c87f4bd4
mikemacharia39 <mikemacharia39@gmail.com>
2023-06-04 23:22:13 +0300

    shuffle string

---
+++ src/javasolutionsforalgorithms/revision_v2/leetcode/google/ShuffleString.java
@@ -0,0 +1,30 @@
+package javasolutionsforalgorithms.revision_v2.leetcode.google;
+
+/**
+ * <a href="https://leetcode.com/problems/shuffle-string/">...</a>
+ *
+ * You are given a string s and an integer array indices of the same length. The string s will be shuffled such that
+ * the character at the ith position moves to indices[i] in the shuffled string.
+ *
+ * Example 1:
+ * Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+ * Output: "leetcode"
+ * Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+ */
+public class ShuffleString {
+
+    /**
+     * Using an array to store the characters in their corresponding position
+     * @param s string input
+     * @param indices array of indices
+     * @return shuffled string
+     */
+    public String restoreString(String s, int[] indices) {
+        char[] arr = new char[indices.length];
+        for (int i = 0; i < indices.length; i++) {
+            int x = indices[i];
+            arr[x] = s.charAt(i);
+        }
+        return new String(arr);
+    }
+}

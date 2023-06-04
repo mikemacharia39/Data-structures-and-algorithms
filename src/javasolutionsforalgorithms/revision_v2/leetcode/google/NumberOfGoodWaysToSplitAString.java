13037d79477b30edbf1efddebd9f23d51f66aa9b
mikemacharia39 <mikemacharia39@gmail.com>
2023-06-04 23:19:24 +0300

    number of good ways to split description

--- src/javasolutionsforalgorithms/revision_v2/leetcode/google/NumberOfGoodWaysToSplitAString.java
+++ src/javasolutionsforalgorithms/revision_v2/leetcode/google/NumberOfGoodWaysToSplitAString.java
@@ -4,6 +4,24 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * <a href="https://leetcode.com/problems/number-of-good-ways-to-split-a-string/">...</a>
+ * You are given a string s.
+ * A split is called good if you can split s into two non-empty strings sleft and sright where their
+ * concatenation is equal to s (i.e., sleft + sright = s) and the number of distinct letters in sleft and sright is the same.
+ * Return the number of good splits you can make in s
+ *
+ * Example 1:
+ *
+ * Input: s = "aacaba"
+ * Output: 2
+ * Explanation: There are 5 ways to split "aacaba" and 2 of them are good.
+ * ("a", "acaba") Left string and right string contains 1 and 3 different letters respectively.
+ * ("aa", "caba") Left string and right string contains 1 and 3 different letters respectively.
+ * ("aac", "aba") Left string and right string contains 2 and 2 different letters respectively (good split).
+ * ("aaca", "ba") Left string and right string contains 2 and 2 different letters respectively (good split).
+ * ("aacab", "a") Left string and right string contains 3 and 1 different letters respectively.
+ */
 public class NumberOfGoodWaysToSplitAString {
 
     /**

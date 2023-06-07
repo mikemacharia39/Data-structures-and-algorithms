a7228b90f295b9e0770e76bd4fb6c69bf99053de
mikemacharia39 <mikemacharia39@gmail.com>
2023-06-07 12:47:52 +0300

    group anagrams solution description

--- src/javasolutionsforalgorithms/revision_v2/leetcode/google/string/GroupAnagrams.java
+++ src/javasolutionsforalgorithms/revision_v2/leetcode/google/string/GroupAnagrams.java
@@ -1,7 +1,17 @@
 package javasolutionsforalgorithms.revision_v2.leetcode.google.string;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+/**
+ * <a href="https://leetcode.com/problems/group-anagrams/">...</a>
+ * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+ * typically using all the original letters exactly once.
+ */
 public class GroupAnagrams {
 
     /**

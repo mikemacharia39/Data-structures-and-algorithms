005948ba17f1dc0770a01fd25564aed717f6417d
mikemacharia39 <mikemacharia39@gmail.com>
2023-06-08 20:22:07 +0300

    most frequent element solution description

--- src/javasolutionsforalgorithms/revision_v2/leetcode/google/arrays/MostFrequentEvenElement.java
+++ src/javasolutionsforalgorithms/revision_v2/leetcode/google/arrays/MostFrequentEvenElement.java
@@ -4,6 +4,20 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.TreeMap;
 
+/**
+ * <a href="https://leetcode.com/problems/most-frequent-even-element/">...</a>
+ *
+ * Given an integer array nums, return the most frequent even element.
+ *
+ * If there is a tie, return the smallest one. If there is no such element, return -1.
+ * Example 1:
+ *
+ * Input: nums = [0,1,2,2,4,4,1]
+ * Output: 2
+ * Explanation:
+ * The even elements are 0, 2, and 4. Of these, 2 and 4 appear the most.
+ * We return the smallest one, which is 2.
+ */
 public class MostFrequentEvenElement {
 
     /**

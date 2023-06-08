975de973191d0654a909cea3020cb02fcaf62643
mikemacharia39 <mikemacharia39@gmail.com>
2023-06-08 13:21:58 +0300

    top k frequent elements, time and space complexity

--- src/javasolutionsforalgorithms/revision_v2/leetcode/google/arrays/TopKFrequentElements.java
+++ src/javasolutionsforalgorithms/revision_v2/leetcode/google/arrays/TopKFrequentElements.java
@@ -5,9 +5,20 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * <a href="https://leetcode.com/problems/top-k-frequent-elements/">...</a>
+ * Given an integer array nums and an integer k, return the k most frequent elements.
+ * You may return the answer in any order.
+ *
+ * Example 1:
+ * Input: nums = [1,1,1,2,2,3], k = 2
+ * Output: [1,2]
+ */
 public class TopKFrequentElements {
     /**
      * Using streams to sort, limit by k, convert  to list and then return an array
+     * Time complexity: O(nlogn). Why? Because of the sorting
+     * Space complexity: O(n). Why? Because of the map
      */
     public int[] topKFrequent_1(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();

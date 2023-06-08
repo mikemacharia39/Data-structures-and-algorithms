2b5381ffab315656c96a6a46de5ccade79895e2c
mikemacharia39 <mikemacharia39@gmail.com>
2023-06-08 20:20:02 +0300

    most frequent element solution 2

--- src/javasolutionsforalgorithms/revision_v2/leetcode/google/arrays/MostFrequentEvenElement.java
+++ src/javasolutionsforalgorithms/revision_v2/leetcode/google/arrays/MostFrequentEvenElement.java
@@ -6,6 +6,30 @@ import java.util.TreeMap;
 
 public class MostFrequentEvenElement {
 
+    public int mostFrequentEven_1(int[] nums) {
+        Map<Integer, Integer> map = new TreeMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            }
+        }
+
+        if (map.isEmpty()) {
+            return - 1;
+        }
+
+        int element = Integer.MIN_VALUE;
+        int maxFreq = Integer.MIN_VALUE;
+        for (int key : map.keySet()) {
+            if (map.get(key) > maxFreq) {
+                element = key;
+                maxFreq = map.get(key);
+            }
+        }
+
+        return element;
+    }
 
     public int mostFrequentEven_2(int[] nums) {
         Map<Integer, Integer> map = new TreeMap<>();

7e1a5186474149469aec98c3a59210024b365ebf
mikemacharia39 <mikemacharia39@gmail.com>
2023-06-10 00:25:33 +0300

    search a 2d array

---
+++ src/javasolutionsforalgorithms/revision_v2/leetcode/google/binarysearch/SearchA2DMatrix.java
@@ -0,0 +1,53 @@
+package javasolutionsforalgorithms.revision_v2.leetcode.google.binarysearch;
+
+public class SearchA2DMatrix {
+
+    /**
+     * Time complexity: O(log(m * n)), because we are doing binary search on a matrix
+     * space complexity: O(n), because we are creating a new array of size n (number of columns
+     * @param matrix 2D matrix
+     * @param target target number to search
+     * @return true if found, false otherwise
+     */
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int foundRow = -1;
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] <= target && matrix[i][cols-1] >= target) {
+                foundRow = i;
+                break;
+            }
+        }
+
+        if (foundRow == -1) return false;
+
+        int[] numbers = new int[cols];
+        for (int j = 0; j < cols; j++) {
+            numbers[j] = matrix[foundRow][j];
+        }
+
+        if (numbers.length == 1) {
+            return numbers[0] == target ? true : false;
+        }
+
+        int left = 0;
+        int right = numbers.length;
+
+        while (left < right) {
+            int mid = left + (right - left)/2;
+
+            if (numbers[mid] == target) {
+                return true;
+            }
+
+            if (numbers[mid] > target) {
+                right = mid;
+            } else if (numbers[mid] < target) {
+                left = mid + 1;
+            }
+        }
+
+        return false;
+    }
+}

08d16eedea40d61102f751ba2c00b03d3762aaca
mikemacharia39 <mikemacharia39@gmail.com>
2023-07-02 11:39:25 +0300

    Longest palindromic substring, more optimized solution

--- src/javasolutionsforalgorithms/revision_v2/leetcode/leetcode/string/LongestPalindromicSubstring.java
+++ src/javasolutionsforalgorithms/revision_v2/leetcode/leetcode/string/LongestPalindromicSubstring.java
@@ -14,7 +14,11 @@ package javasolutionsforalgorithms.revision_v2.leetcode.leetcode.string;
  */
 public class LongestPalindromicSubstring {
 
-    public String longestPalindrome(String s) {
+    /**
+     * This is a brute force solution. We will generate all the substrings and check if they are palindrome.
+     * Time complexity: O(n^3)
+     */
+    public String longestPalindrome1(String s) {
         int n = s.length();
         String longest = "";
         for (int i = 0; i < n; i++) {
@@ -43,5 +47,64 @@ public class LongestPalindromicSubstring {
         }
 
         return true;
+    }
+
+    /**
+     * Time complexity O(n^2)
+     * Explanation: The idea is to expand around the center of the palindrome. There are two types of palindromes:
+     * - odd length palindrome: aba
+     * - even length palindrome: abba
+     * For each character in the string, we will expand around the center of the palindrome. The center of the palindrome
+     * can be either the character itself or the character and the next character.
+     * For example: aba, the center of the palindrome is b. For abba, the center of the palindrome is bb.
+     * We will expand around the center of the palindrome and check if the left and right characters are equal.
+     * If they are equal, we will expand further. If they are not equal, we will break out of the loop.
+     * We will keep track of the longest palindrome and return it.
+     * For In depth explanation visit: https://www.youtube.com/watch?v=UflHuQj6MVA
+     * @param s string
+     * @return longest palindrome
+     */
+    public String longestPalindrome(String s) {
+        // more optimized solution
+        int n = s.length();
+        String longest = "";
+        for (int i = 0; i < n; i++) {
+            String odd = expandAroundCenter(s, i, i);
+            String even = expandAroundCenter(s, i, i + 1);
+
+            if (odd.length() > longest.length()) {
+                longest = odd;
+            }
+
+            if (even.length() > longest.length()) {
+                longest = even;
+            }
+        }
+
+        return longest;
+    }
+
+    /**
+     *
+     * @param s
+     * @param i
+     * @param i1
+     * @return
+     */
+    private String expandAroundCenter(String s, int i, int i1) {
+        // i and i1 are the center of the palindrome
+        int left = i;
+        int right = i1;
+
+        while (left >= 0 && right < s.length()) {
+            if (s.charAt(left) != s.charAt(right)) {
+                break;
+            }
+
+            left--;
+            right++;
+        }
+
+        return s.substring(left + 1, right);
     }
 }

b02ef07ebd196cb461ff8716417cd5197765f514
mikemacharia39 <mikemacharia39@gmail.com>
2023-07-02 10:30:06 +0300

    convert array into 2d list, description

--- src/javasolutionsforalgorithms/revision_v2/leetcode/leetcode/arrays/ConvertArrayInto2DArrayWithConditions.java
+++ src/javasolutionsforalgorithms/revision_v2/leetcode/leetcode/arrays/ConvertArrayInto2DArrayWithConditions.java
@@ -10,20 +10,17 @@ import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * <a href="https://leetcode.com/problems/convert-an-array-into-a-2d-array-with-conditions/">...</a>
- *
+ * <p>
  * You are given an integer array nums. You need to create a 2D array from nums satisfying the following conditions:
- *
+ * <p>
  * The 2D array should contain only the elements of the array nums.
  * Each row in the 2D array contains distinct integers.
  * The number of rows in the 2D array should be minimal.
  * Return the resulting array. If there are multiple answers, return any of them.
- *
+ * <p>
  * Note that the 2D array can have a different number of elements on each row.
  *
- *
- *
  * Example 1:
- *
  * Input: nums = [1,3,4,1,2,3,1]
  * Output: [[1,3,4,2],[1,3],[1]]
  * Explanation: We can create a 2D array that contains the following rows:

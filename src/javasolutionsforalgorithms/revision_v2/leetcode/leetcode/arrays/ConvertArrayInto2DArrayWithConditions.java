49b28b2f5b862505e62ba7b1f713635bb9a667eb
mikemacharia39 <mikemacharia39@gmail.com>
2023-07-02 10:29:22 +0300

    convert array into 2d list, description

--- src/javasolutionsforalgorithms/revision_v2/leetcode/leetcode/arrays/ConvertArrayInto2DArrayWithConditions.java
+++ src/javasolutionsforalgorithms/revision_v2/leetcode/leetcode/arrays/ConvertArrayInto2DArrayWithConditions.java
@@ -8,13 +8,36 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * <a href="https://leetcode.com/problems/convert-an-array-into-a-2d-array-with-conditions/">...</a>
+ *
+ * You are given an integer array nums. You need to create a 2D array from nums satisfying the following conditions:
+ *
+ * The 2D array should contain only the elements of the array nums.
+ * Each row in the 2D array contains distinct integers.
+ * The number of rows in the 2D array should be minimal.
+ * Return the resulting array. If there are multiple answers, return any of them.
+ *
+ * Note that the 2D array can have a different number of elements on each row.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,3,4,1,2,3,1]
+ * Output: [[1,3,4,2],[1,3],[1]]
+ * Explanation: We can create a 2D array that contains the following rows:
+ * - 1,3,4,2
+ * - 1,3
+ * - 1
+ * All elements of nums were used, and each row of the 2D array contains distinct integers, so it is a valid answer.
+ * It can be shown that we cannot have less than 3 rows in a valid array.
+ */
 public class ConvertArrayInto2DArrayWithConditions {
 
     /**
      * Time complexity: O(n)
-     * Space complexity: O(n)
-     * @param nums
-     * @return
+     * Space complexity: O(n): map
      */
     public List<List<Integer>> findMatrix(int[] nums) {
 
@@ -53,9 +76,7 @@ public class ConvertArrayInto2DArrayWithConditions {
 
     /**
      * Time complexity: O(n)
-     * Space complexity: O(n)
-     * @param nums
-     * @return
+     * Space complexity: O(n): map + set
      */
     public List<List<Integer>> findMatrix1(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();

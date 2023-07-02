93b83904f08633482859228b63a84f99d5bf481d
mikemacharia39 <mikemacharia39@gmail.com>
2023-07-02 11:24:48 +0300

    Longest palindromic substring

---
+++ src/javasolutionsforalgorithms/revision_v2/leetcode/leetcode/string/LongestPalindromicSubstring.java
@@ -0,0 +1,31 @@
+package javasolutionsforalgorithms.revision_v2.leetcode.leetcode.string;
+
+/**
+ * Given a string s, return the longest palindromic substring in s.
+ * Example 1:
+ *
+ * Input: s = "babad"
+ * Output: "bab"
+ * Explanation: "aba" is also a valid answer.
+ * Example 2:
+ *
+ * Input: s = "cbbd"
+ * Output: "bb"
+ */
+public class LongestPalindromicSubstring {
+
+    public boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}

6e441584bb523c3bb9767a84c6dc89ff16837351
mikemacharia39 <mikemacharia39@gmail.com>
2023-10-15 20:08:18 +0300

    Minimum Sum of Four Digit Number After Splitting Digits, examples

--- src/javasolutionsforalgorithms/revision_v2/leetcode/math/MinimumSumofFourDigitNumberAfterSplitting.java
+++ src/javasolutionsforalgorithms/revision_v2/leetcode/math/MinimumSumofFourDigitNumberAfterSplitting.java
@@ -11,6 +11,20 @@ import java.util.Arrays;
  * For example, given num = 2932, you have the following digits: two 2's, one 9 and one 3. Some of the possible
  * pairs [new1, new2] are [22, 93], [23, 92], [223, 9] and [2, 329].
  * Return the minimum possible sum of new1 and new2.
+ *
+ * Example 1:
+ *
+ * Input: num = 2932
+ * Output: 52
+ * Explanation: Some possible pairs [new1, new2] are [29, 23], [223, 9], etc.
+ * The minimum sum can be obtained by the pair [29, 23]: 29 + 23 = 52.
+ *
+ * Example 2:
+ *
+ * Input: num = 4009
+ * Output: 13
+ * Explanation: Some possible pairs [new1, new2] are [0, 49], [490, 0], etc.
+ * The minimum sum can be obtained by the pair [4, 9]: 4 + 9 = 13.
  */
 public class MinimumSumofFourDigitNumberAfterSplitting {
     // these is greedy approach

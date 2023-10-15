9997814c4284e17d7051bdd5b4fa6298b2291e54
mikemacharia39 <mikemacharia39@gmail.com>
2023-10-15 19:58:55 +0300

    mysql question

---
+++ src/databases_concepts/mysql/leetcode/mysql_questions.sql
@@ -0,0 +1,13 @@
+-- Number of Unique Subjects Taught by Each Teacher
+-- SQL Schema
+-- Table: Teacher
+--
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | teacher_id  | int  |
+-- | subject_id  | int  |
+-- | dept_id     | int  |
+--+-------------+------+
+-- (subject_id, dept_id) is the primary key (combinations of columns with unique values) of this table.
+-- Each row in this table indicates that the teacher with teacher_id teaches the subject subject_id in the department dept_id.

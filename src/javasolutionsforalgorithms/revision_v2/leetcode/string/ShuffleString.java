a67d8e33f9583c508a8eaeaede30c9827c868606
mikemacharia39 <mikemacharia39@gmail.com>
2023-10-15 19:55:18 +0300

    Shuffle string url

--- src/javasolutionsforalgorithms/revision_v2/leetcode/string/ShuffleString.java
+++ src/javasolutionsforalgorithms/revision_v2/leetcode/string/ShuffleString.java
@@ -1,6 +1,8 @@
 package javasolutionsforalgorithms.revision_v2.leetcode.string;
 
 /**
+ * <a href="https://leetcode.com/problems/shuffle-string/">...</a>
+ *
  * You are given a string s and an integer array indices of the same length. The string s will be shuffled such that
  * the character at the ith position moves to indices[i] in the shuffled string.
  *

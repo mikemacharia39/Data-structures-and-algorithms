e23a0a20629d427839703ea895aae28dee75814b
mikemacharia39 <mikemacharia39@gmail.com>
2023-10-15 19:59:24 +0300

    mysql question, answer

--- src/databases_concepts/mysql/leetcode/mysql_questions.sql
+++ src/databases_concepts/mysql/leetcode/mysql_questions.sql
@@ -11,3 +11,6 @@
 --+-------------+------+
 -- (subject_id, dept_id) is the primary key (combinations of columns with unique values) of this table.
 -- Each row in this table indicates that the teacher with teacher_id teaches the subject subject_id in the department dept_id.
+
+-- Write your MySQL query statement below
+SELECT teacher_id, COUNT(DISTINCT(subject_id)) AS cnt FROM Teacher GROUP BY teacher_id;

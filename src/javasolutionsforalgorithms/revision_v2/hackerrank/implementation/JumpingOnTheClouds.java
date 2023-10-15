2f91f4c40ce0f1695c2acd5d3db4d935549a1f55
mikemacharia39 <mikemacharia39@gmail.com>
2023-10-15 21:01:20 +0300

    Total jumps

---
+++ src/javasolutionsforalgorithms/revision_v2/hackerrank/implementation/JumpingOnTheClouds.java
@@ -0,0 +1,28 @@
+package javasolutionsforalgorithms.revision_v2.hackerrank.implementation;
+
+import java.util.List;
+
+/**
+ * There is a new mobile game that starts with consecutively numbered clouds.
+ * Some of the clouds are thunderheads and others are cumulus.
+ * The player can jump on any cumulus cloud having a number that is equal to the number of the current cloud plus 1 or 2.
+ * The player must avoid the thunderheads. Determine the minimum number of jumps it will take to jump from the starting
+ * postion to the last cloud. It is always possible to win the game.
+ *
+ * For each game, you will get an array of clouds numbered  if they are safe or 1 if they must be avoided.
+ *
+ * Sample Input 0
+ *
+ * 0 0 1 0 0 1 0
+ * Sample Output 0
+ *
+ * Result = 4
+ * Explanation: 0 -> 2 -> 4 -> 6
+ */
+public class JumpingOnTheClouds {
+
+    public static int jumpingOnClouds(List<Integer> c) {
+        int totalJumps = 0;
+
+    }
+}

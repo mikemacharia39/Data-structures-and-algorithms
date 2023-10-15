9e194b374427cd8ed14265b1fc1467d112b39b17
mikemacharia39 <mikemacharia39@gmail.com>
2023-10-15 20:22:26 +0300

    Calculate Digit Sum of a String, description

--- src/javasolutionsforalgorithms/revision_v2/leetcode/math/CalculateDigitSumOfAString.java
+++ src/javasolutionsforalgorithms/revision_v2/leetcode/math/CalculateDigitSumOfAString.java
@@ -19,7 +19,7 @@ package javasolutionsforalgorithms.revision_v2.leetcode.math;
  * Output: "135"
  * Explanation:
  * - For the first round, we divide s into groups of size 3: "111", "112", "222", and "23".
- *   ​​​​​Then we calculate the digit sum of each group: 1 + 1 + 1 = 3, 1 + 1 + 2 = 4, 2 + 2 + 2 = 6, and 2 + 3 = 5.
+ *   Then we calculate the digit sum of each group: 1 + 1 + 1 = 3, 1 + 1 + 2 = 4, 2 + 2 + 2 = 6, and 2 + 3 = 5.
  *   So, s becomes "3" + "4" + "6" + "5" = "3465" after the first round.
  * - For the second round, we divide s into "346" and "5".
  *   Then we calculate the digit sum of each group: 3 + 4 + 6 = 13, 5 = 5.

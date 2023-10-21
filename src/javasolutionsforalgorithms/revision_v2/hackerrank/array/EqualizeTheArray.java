14a437b8bc043b7b01865a68256994e2a4e86009
mikemacharia39 <mikemacharia39@gmail.com>
2023-10-21 06:55:16 +0300

    Equalize the array description

---
+++ src/javasolutionsforalgorithms/revision_v2/hackerrank/array/EqualizeTheArray.java
@@ -0,0 +1,24 @@
+package javasolutionsforalgorithms.revision_v2.hackerrank.array;
+
+/**
+ * Given an array of integers, determine the minimum number of elements to delete to leave only elements of equal value.
+ *
+ * Example
+ * arr = {1,2,2,3}
+ *
+ * Delete the 2 elements 1 and 3 leaving arr = [2,2].
+ * If both twos plus either the 1 or the 3 are deleted, it takes 2 deletions to leave either 3 or 1.
+ * The minimum number of deletions is 2.
+ *
+ * Sample Input
+ *
+ * STDIN       Function
+ * -----       --------
+ * 3 3 2 1 3   arr = [3, 3, 2, 1, 3]
+ *
+ * Sample Output = 2, because we deleted 2,1 and the arr = [3,3,3]
+ *
+ */
+public class EqualizeTheArray {
+
+}

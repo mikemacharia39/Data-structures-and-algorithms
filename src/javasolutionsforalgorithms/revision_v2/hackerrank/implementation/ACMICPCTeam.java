670bb98112f0f6390675565582a34da6f6bdcb97
mikemacharia39 <mikemacharia39@gmail.com>
2023-10-23 21:51:35 +0300

    Hackerrank AIPC Team

---
+++ src/javasolutionsforalgorithms/revision_v2/hackerrank/implementation/ACMICPCTeam.java
@@ -0,0 +1,15 @@
+package javasolutionsforalgorithms.revision_v2.hackerrank.implementation;
+
+/**
+ * <a href="https://www.hackerrank.com/challenges/acm-icpc-team/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign">...</a>
+ *
+ * There are a number of people who will be attending ACM-ICPC World Finals.
+ * Each of them may be well versed in a number of topics. Given a list of topics known by each attendee,
+ * presented as binary strings, determine the maximum number of topics a 2-person team can know.
+ * Each subject has a column in the binary string, and a '1' means the subject is known while '0' means it is not.
+ * Also determine the number of teams that know the maximum number of topics. Return an integer array with two elements.
+ * The first is the maximum number of topics known, and the second is the number of teams that know that number of topics.
+ */
+public class ACMICPCTeam {
+
+}

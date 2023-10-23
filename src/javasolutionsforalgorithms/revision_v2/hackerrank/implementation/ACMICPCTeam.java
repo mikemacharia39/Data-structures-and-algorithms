f3e2aefffc208a7fbfb94f6ee29d767bc13b860c
mikemacharia39 <mikemacharia39@gmail.com>
2023-10-23 21:54:31 +0300

    Hackerrank AIPC Team, helper function

--- src/javasolutionsforalgorithms/revision_v2/hackerrank/implementation/ACMICPCTeam.java
+++ src/javasolutionsforalgorithms/revision_v2/hackerrank/implementation/ACMICPCTeam.java
@@ -9,7 +9,36 @@ package javasolutionsforalgorithms.revision_v2.hackerrank.implementation;
  * Each subject has a column in the binary string, and a '1' means the subject is known while '0' means it is not.
  * Also determine the number of teams that know the maximum number of topics. Return an integer array with two elements.
  * The first is the maximum number of topics known, and the second is the number of teams that know that number of topics.
+ *
+ * The attendee data is aligned for clarity below:
+ * Example: topic = ["10101", "11110", "00010""]
+ * 10101
+ * 11110
+ * 00010
+ *
+ * These are all possible teams that can be formed:
+ *
+ * Members | Subjects
+ * (1,2)     [1,2,3,4,5]
+ * (1,3)     [1,3,4,5]
+ * (2,3)    [1,2,3,4]
+ * In this case, the first team will know all 5 subjects. They are the only team that can be created that knows that
+ * many subjects, so [5,1]  is returned.
  */
 public class ACMICPCTeam {
+
+
+    private static int countTeams(String team1, String team2) {
+        int knownTopics = 0;
+        int teamSize = team1.length();
+
+        for (int i = 0; i < teamSize; i++) {
+            if (team1.charAt(i) == '1' || team2.charAt(i) == '1') {
+                knownTopics++;
+            }
+        }
+
+        return knownTopics;
+    }
 
 }

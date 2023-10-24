aced5a27bc6de836f0321de2d898ec989d3d158a
mikemacharia39 <mikemacharia39@gmail.com>
2023-10-25 00:00:59 +0300

    sales by match, example

--- src/javasolutionsforalgorithms/revision_v2/hackerrank/maps/SalesByMatch.java
+++ src/javasolutionsforalgorithms/revision_v2/hackerrank/maps/SalesByMatch.java
@@ -5,6 +5,11 @@ package javasolutionsforalgorithms.revision_v2.hackerrank.maps;
  *
  * There is a large pile of socks that must be paired by color. Given an array of integers representing the color of each sock,
  * determine how many pairs of socks with matching colors there are.
+ *
+ * Example
+ * n = 7
+ * ar = [1,2,1,2,1,3,2]
+ * There is one pair of 1 and one of 2. There are three odd socks left, one of each color. The number of pairs is 2.
  */
 public class SalesByMatch {
 

cc1f9dd9933f3ed0402cdae001d2b86a662ee1f2
mikemacharia39 <mikemacharia39@gmail.com>
2023-10-24 23:59:54 +0300

    sales by match

---
+++ src/javasolutionsforalgorithms/revision_v2/hackerrank/maps/SalesByMatch.java
@@ -0,0 +1,12 @@
+package javasolutionsforalgorithms.revision_v2.hackerrank.maps;
+
+/**
+ * <a href="https://www.hackerrank.com/challenges/sock-merchant/problem">...</a>
+ *
+ * There is a large pile of socks that must be paired by color. Given an array of integers representing the color of each sock,
+ * determine how many pairs of socks with matching colors there are.
+ */
+public class SalesByMatch {
+
+
+}

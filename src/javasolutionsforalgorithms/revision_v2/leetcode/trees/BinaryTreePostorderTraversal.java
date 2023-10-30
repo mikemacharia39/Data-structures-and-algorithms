84f164ac602fbb951a32fdd26f65f11fd81d4e70
mikemacharia39 <mikemacharia39@gmail.com>
2023-10-30 22:19:50 +0300

    post order traversal, solution

--- src/javasolutionsforalgorithms/revision_v2/leetcode/trees/BinaryTreePostorderTraversal.java
+++ src/javasolutionsforalgorithms/revision_v2/leetcode/trees/BinaryTreePostorderTraversal.java
@@ -1,5 +1,8 @@
 package javasolutionsforalgorithms.revision_v2.leetcode.trees;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * <a href="https://leetcode.com/problems/binary-tree-postorder-traversal/">...</a>
  * Given the root of a binary tree, return the postorder traversal of its nodes' values.
@@ -14,4 +17,37 @@ package javasolutionsforalgorithms.revision_v2.leetcode.trees;
  * Output: [3,2,1]
  */
 public class BinaryTreePostorderTraversal {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+
+        traverse(root, list);
+
+        return list;
+    }
+
+    private void traverse(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        traverse(root.left, list);
+        traverse(root.right, list);
+        list.add(root.val);
+    }
 }

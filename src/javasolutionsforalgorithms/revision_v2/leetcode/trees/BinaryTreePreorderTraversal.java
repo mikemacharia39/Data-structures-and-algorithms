37f8354fb82e7dacc89910d6e53d6723b78cc302
mikemacharia39 <mikemacharia39@gmail.com>
2023-10-30 22:28:18 +0300

    pre order traversal

---
+++ src/javasolutionsforalgorithms/revision_v2/leetcode/trees/BinaryTreePreorderTraversal.java
@@ -0,0 +1,37 @@
+package javasolutionsforalgorithms.revision_v2.leetcode.trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <a href="https://leetcode.com/problems/binary-tree-preorder-traversal/">...</a>
+ * Given the root of a binary tree, return the preorder traversal of its nodes' values.
+ */
+public class BinaryTreePreorderTraversal {
+
+    private final List<Integer> list = new ArrayList<>();
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if (root == null) {
+            return list;
+        }
+
+        list.add(root.val);
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+        return list;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}

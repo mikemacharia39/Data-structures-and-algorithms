af5275a652335bc4590febba49f533e05232f0b3
mikemacharia39 <mikemacharia39@gmail.com>
2023-10-30 22:13:18 +0300

    post order traversal, link

--- src/javasolutionsforalgorithms/revision_v2/leetcode/trees/BinaryTreePostorderTraversal.java
+++ src/javasolutionsforalgorithms/revision_v2/leetcode/trees/BinaryTreePostorderTraversal.java
@@ -1,8 +1,8 @@
 package javasolutionsforalgorithms.revision_v2.leetcode.trees;
 
 /**
+ * <a href="https://leetcode.com/problems/binary-tree-postorder-traversal/">...</a>
  * Given the root of a binary tree, return the postorder traversal of its nodes' values.
-
  * Example 1:
  *              1
  *                \

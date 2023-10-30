18255a4bd37f78e5ab0ecec8dbcf5eeac7d3807a
mikemacharia39 <mikemacharia39@gmail.com>
2023-10-30 21:16:06 +0300

    Increasing BST, solution 1 using inorder traversal

--- src/javasolutionsforalgorithms/revision_v2/leetcode/trees/IncreasingOrderSearchTree.java
+++ src/javasolutionsforalgorithms/revision_v2/leetcode/trees/IncreasingOrderSearchTree.java
@@ -1,5 +1,8 @@
 package javasolutionsforalgorithms.revision_v2.leetcode.trees;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Given the root of a binary search tree, rearrange the tree in in-order so that the leftmost node in the tree is now
  * the root of the tree, and every node has no left child and only one right child.
@@ -21,5 +24,37 @@ public class IncreasingOrderSearchTree {
         TreeNode right;
         TreeNode() {}
         TreeNode(int val) {this.val = val;}
+    }
+
+    public TreeNode increasingBST(TreeNode root) {
+        TreeNode ansRoot = new TreeNode(0);
+        TreeNode ans = ansRoot;
+
+        List<Integer> list = new ArrayList<>();
+        inorder(root, list);
+
+        for (int val : list) {
+            ans.right = new TreeNode(val);
+            ans = ans.right;
+        }
+
+        return ansRoot.right;
+    }
+
+    /**
+     * Inorder traversal of a BST gives a sorted list
+     * LEFT -> ROOT -> RIGHT
+     * Time = O(N)
+     * @param root
+     * @param list
+     */
+    private void inorder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
     }
 }

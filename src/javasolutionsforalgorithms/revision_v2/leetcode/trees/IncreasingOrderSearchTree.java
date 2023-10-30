4d46ceeb008650d5f1fb11be577d70b4f7dbb661
mikemacharia39 <mikemacharia39@gmail.com>
2023-10-30 21:18:43 +0300

    Increasing BST, solution 2 using inorder traversal with recursion

--- src/javasolutionsforalgorithms/revision_v2/leetcode/trees/IncreasingOrderSearchTree.java
+++ src/javasolutionsforalgorithms/revision_v2/leetcode/trees/IncreasingOrderSearchTree.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * <a href="https://leetcode.com/problems/increasing-order-search-tree/">...</a>
+ *
  * Given the root of a binary search tree, rearrange the tree in in-order so that the leftmost node in the tree is now
  * the root of the tree, and every node has no left child and only one right child.
  *

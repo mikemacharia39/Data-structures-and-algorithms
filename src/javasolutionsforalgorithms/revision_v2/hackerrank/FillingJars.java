971ebf60a09c1ed08d2bc1ee583f704516488039
mikemacharia39 <mikemacharia39@gmail.com>
2023-12-12 15:53:27 +0300

    Filling Jars

---
+++ src/javasolutionsforalgorithms/revision_v2/hackerrank/FillingJars.java
@@ -0,0 +1,15 @@
+package javasolutionsforalgorithms.revision_v2.hackerrank;
+
+/**
+ * <a href="https://www.hackerrank.com/challenges/filling-jars/problem">...</a>
+ *
+ * Animesh has n empty candy jars, numbered from 1 to n, with infinite capacity.
+ * He performs m operations. Each operation is described by  integers, a, b, and k.
+ * Here, a and b are indices of the jars, and k is the number of candies to be added inside each jar whose index lies
+ * between a and b (both inclusive). Can you tell the average number of candies after m operations?
+ *
+ */
+public class FillingJars {
+
+
+}

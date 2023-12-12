f28131f3b1415529449c6e07e28f7b881293fbe9
mikemacharia39 <mikemacharia39@gmail.com>
2023-12-12 15:56:01 +0300

    Filling Jars, solutions

--- src/javasolutionsforalgorithms/revision_v2/hackerrank/FillingJars.java
+++ src/javasolutionsforalgorithms/revision_v2/hackerrank/FillingJars.java
@@ -1,5 +1,7 @@
 package javasolutionsforalgorithms.revision_v2.hackerrank;
 
+import java.util.List;
+
 /**
  * <a href="https://www.hackerrank.com/challenges/filling-jars/problem">...</a>
  *
@@ -11,5 +13,48 @@ package javasolutionsforalgorithms.revision_v2.hackerrank;
  */
 public class FillingJars {
 
+    /**
+     * Time complexity: O(n)
+     * @param n
+     * @param operations
+     * @return
+     */
+    public static int solve(int n, List<List<Integer>> operations) {
+        double sum = 0;
+        double times = 0;
+        double average = 0;
+        for (List<Integer> operation : operations) {
+            int a = operation.get(0);
+            int b = operation.get(1);
+            int k = operation.get(2);
+            times = b - a + 1;
+            sum = k * times;
+            average += sum / n;
+        }
 
+        return (int)average;
+    }
+
+    /**
+     * Time complexity: O(n*m)
+     * Easy to understand but times out for large inputs
+     * @param n
+     * @param operations
+     * @return
+     */
+    public static int solve1(int n, List<List<Integer>> operations) {
+        int[] result = new int[n+1];
+
+        for (List<Integer> list : operations) {
+            for (int i = list.get(0); i <= list.get(1); i++) {
+                result[i] += list.get(2);
+            }
+        }
+
+        int sum = 0;
+        for (int number : result) {
+            sum += number;
+        }
+        return sum/n;
+    }
 }

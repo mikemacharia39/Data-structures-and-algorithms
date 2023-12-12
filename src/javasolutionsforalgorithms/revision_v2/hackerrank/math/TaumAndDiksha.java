c875d1a919ea2281eb4cebf78577b0f0e5b1b73d
mikemacharia39 <mikemacharia39@gmail.com>
2023-12-12 16:56:11 +0300

    Taum and Diksha birthday, link

--- src/javasolutionsforalgorithms/revision_v2/hackerrank/math/TaumAndDiksha.java
+++ src/javasolutionsforalgorithms/revision_v2/hackerrank/math/TaumAndDiksha.java
@@ -1,6 +1,8 @@
 package javasolutionsforalgorithms.revision_v2.hackerrank.math;
 
 /**
+ * <a href="https://www.hackerrank.com/challenges/taum-and-bday/problem">...</a>
+ *
  * Taum is planning to celebrate the birthday of his friend, Diksha.
  * There are two types of gifts that Diksha wants from Taum: one is black and the other is white.
  * To make her happy, Taum has to buy b black gifts and w white gifts.

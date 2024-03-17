9af7b2db3ac6e3cf59c30c2b2907dcf9923e80f0
mikemacharia39 <mikemacharia39@gmail.com>
2024-03-17 18:54:41 +0300

    chocolate feast, link

--- src/javasolutionsforalgorithms/revision_v2/hackerrank/math/ChocolateFeast.java
+++ src/javasolutionsforalgorithms/revision_v2/hackerrank/math/ChocolateFeast.java
@@ -1,6 +1,8 @@
 package javasolutionsforalgorithms.revision_v2.hackerrank.math;
 
 /**
+ * <a href="https://www.hackerrank.com/challenges/chocolate-feast/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign">...</a>
+ *
  * Little Bobby loves chocolate. He frequently goes to his favorite 5&10 store, Penny Auntie, to buy them.
  * They are having a promotion at Penny Auntie. If Bobby saves enough wrappers, he can turn them in for a free chocolate.
  *

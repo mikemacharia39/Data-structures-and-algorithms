34975fffd12040fef4001d8e47042f4e647116ce
mikemacharia39 <mikemacharia39@gmail.com>
2024-03-17 15:14:32 +0300

    Closest number, example

--- src/javasolutionsforalgorithms/revision_v2/hackerrank/math/ClosestNumbers.java
+++ src/javasolutionsforalgorithms/revision_v2/hackerrank/math/ClosestNumbers.java
@@ -6,6 +6,16 @@ package javasolutionsforalgorithms.revision_v2.hackerrank.math;
  * The most common task is to make finding things easier, but there are other uses as well.
  * In this case, it will make it easier to determine which pair or pairs of elements have the smallest absolute
  * difference between them.
+ *
+ * Example 1:
+ * arr = [-20, -3916237, -357920, -3620601, 7374819, -7330761, 30, 6246457, -6461594, 266854]
+ *
+ * answer = [-20, 30]
+ *
+ * Example 2:
+ * arr = [3, 2, 1, 5, 4]
+ *
+ * answer = [1, 2, 2, 3, 3, 4, 4, 5]
  */
 public class ClosestNumbers {
 }

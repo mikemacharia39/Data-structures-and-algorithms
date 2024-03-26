bda9ee28e517d87e2d2b408f1ef2867a71a97ae6
mikemacharia39 <mikemacharia39@gmail.com>
2024-03-26 18:53:28 +0300

    flipping bits, solution 2

--- src/javasolutionsforalgorithms/revision_v2/hackerrank/math/FlippingBits.java
+++ src/javasolutionsforalgorithms/revision_v2/hackerrank/math/FlippingBits.java
@@ -8,7 +8,49 @@ package javasolutionsforalgorithms.revision_v2.hackerrank.math;
  */
 public class FlippingBits {
 
-    public static long flippingBits(long n) {
+    // Solution 1
+
+    public static long flippingBits1(long n) {
         return n ^ ((1L << 32) - 1); // XOR with 32 bits of 1
+    }
+
+    // Solution 2
+
+    public static long flippingBits(long n) {
+
+        long[] bits = new long[32];
+
+        String toBinary = longToBinary(n);
+
+        int toBinaryLen = toBinary.length() - 1;
+        for (int i = bits.length - 1; i >= (bits.length - toBinary.length()); i--) {
+            bits[i] = toBinary.charAt(toBinaryLen) - '0';
+            toBinaryLen--;
+        }
+
+        for (int j = bits.length - 1; j >= 0; j--) {
+            long newBit = (bits[j] + 1) % 2;
+            bits[j] = newBit;
+        }
+
+        return binaryToLong(bits);
+    }
+
+    private static long binaryToLong(long[] bits) {
+
+        long sum = 0;
+        for (long bit : bits) {
+            sum = (2 * sum) + bit;
+        }
+
+        return sum;
+    }
+
+    private static String longToBinary(long n) {
+        if (n <= 1) {
+            return Long.toString(n);
+        }
+
+        return longToBinary(n/2) + n % 2;
     }
 }

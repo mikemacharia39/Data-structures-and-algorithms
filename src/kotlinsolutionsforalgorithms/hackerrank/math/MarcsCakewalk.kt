18b28f5534620245cb1886f3fe1ef0ee6cf387c3
mikemacharia39 <mikemacharia39@gmail.com>
2024-03-26 12:30:28 +0300

    Marcs Cakewalk in kotlin

--- src/kotlinsolutionsforalgorithms/hackerrank/math/MarcsCakewalk.kt
+++ src/kotlinsolutionsforalgorithms/hackerrank/math/MarcsCakewalk.kt
@@ -2,6 +2,17 @@ package kotlinsolutionsforalgorithms.hackerrank.math
 
 import kotlin.math.pow
 
+/**
+ * <a href="https://www.hackerrank.com/challenges/marcs-cakewalk/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign">...</a>
+ *
+ * Marc loves cupcakes, but he also likes to stay fit. Each cupcake has a calorie count,
+ * and Marc can walk a distance to expend those calories.
+ * If Marc has eaten  cupcakes so far, after eating a cupcake with c calories he must walk at least 2^j * c miles to maintain his weight.
+ *
+ * Example:
+ * calories = [5, 10, 7]
+ * answer = 44, because the minimum miles he must walk is 2^0 * 10 + 2^1 * 7 + 2^2 * 5 = 44
+ */
 fun main() {
     marcsCakewalk(listOf(5,10,7)).also(::println)
     println("=======================")

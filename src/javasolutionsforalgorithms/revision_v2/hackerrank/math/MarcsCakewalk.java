841aeb02eb22b22b1bac99ee89be51f09f3d62f2
mikemacharia39 <mikemacharia39@gmail.com>
2024-03-26 12:08:24 +0300

    Marcs Cakewalk solution

--- src/javasolutionsforalgorithms/revision_v2/hackerrank/math/MarcsCakewalk.java
+++ src/javasolutionsforalgorithms/revision_v2/hackerrank/math/MarcsCakewalk.java
@@ -1,5 +1,8 @@
 package javasolutionsforalgorithms.revision_v2.hackerrank.math;
 
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * Marc loves cupcakes, but he also likes to stay fit. Each cupcake has a calorie count,
  * and Marc can walk a distance to expend those calories.
@@ -10,4 +13,13 @@ package javasolutionsforalgorithms.revision_v2.hackerrank.math;
  * answer = 44, because the minimum miles he must walk is 2^0 * 10 + 2^1 * 7 + 2^2 * 5 = 44
  */
 public class MarcsCakewalk {
+    public static long marcsCakewalk(List<Integer> calorie) {
+        calorie.sort(Comparator.reverseOrder());
+        long sum = 0;
+        for (int i = 0; i < calorie.size(); i++) {
+            sum += (long) (Math.pow(2,i) * calorie.get(i));
+        }
+
+        return sum;
+    }
 }

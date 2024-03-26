e9635fe7a875cc7768626964a9fa256643bbe2a8
mikemacharia39 <mikemacharia39@gmail.com>
2024-03-26 18:52:05 +0300

    flipping bits, solution 1

--- src/javasolutionsforalgorithms/revision_v2/hackerrank/math/FlippingBits.java
+++ src/javasolutionsforalgorithms/revision_v2/hackerrank/math/FlippingBits.java
@@ -7,4 +7,8 @@ package javasolutionsforalgorithms.revision_v2.hackerrank.math;
  * an unsigned integer.
  */
 public class FlippingBits {
+
+    public static long flippingBits(long n) {
+        return n ^ ((1L << 32) - 1); // XOR with 32 bits of 1
+    }
 }

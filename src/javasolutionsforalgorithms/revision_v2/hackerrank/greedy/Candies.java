7a42a4fee0fb10902524c92f5f68fc71dbcaa884
mikemacharia39 <mikemacharia39@gmail.com>
2024-04-10 20:23:15 +0300

    Candies solution

--- src/javasolutionsforalgorithms/revision_v2/hackerrank/greedy/Candies.java
+++ src/javasolutionsforalgorithms/revision_v2/hackerrank/greedy/Candies.java
@@ -1,5 +1,7 @@
 package javasolutionsforalgorithms.revision_v2.hackerrank.greedy;
 
+import java.util.List;
+
 /**
  * Alice is a kindergarten teacher. She wants to give some candies to the children in her class.
  * All the children sit in a line and each of them has a rating score according to his or her performance in the class.
@@ -13,5 +15,39 @@ package javasolutionsforalgorithms.revision_v2.hackerrank.greedy;
  * Answer = [1, 2, 1, 2, 3, 1]. She must buy a minimum of 10 candies.
  */
 public class Candies {
+
+    /**
+     * Explanation: The solution is to create two arrays left and right. The left array stores the number of candies
+     * given to the left child if the left child has a higher rating than the right child. The right array stores the
+     * number of candies given to the right child if the right child has a higher rating than the left child.
+     * The answer is the maximum of the left and right arrays.
+     *
+     * Time complexity = O(N)
+     * Space complexity = O(N) because of left and right arrays
+     * @param n
+     * @param arr
+     * @return
+     */
+    public static long candies(int n, List<Integer> arr) {
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        left[0] = 1;
+        for (int i = 1; i < n; i++) {
+            left[i] = arr.get(i) > arr.get(i - 1) ? left[i - 1] + 1 : 1;
+        }
+
+        right[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = arr.get(i) > arr.get(i + 1) ? right[i + 1] + 1 : 1;
+        }
+
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += Math.max(left[i], right[i]);
+        }
+
+        return sum;
+    }
 
 }

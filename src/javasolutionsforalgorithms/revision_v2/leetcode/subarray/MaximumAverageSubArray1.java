1d691a62833eaef4e8c06f41a9253317b6426c5e
mikemacharia39 <mikemacharia39@gmail.com>
2024-04-21 23:35:17 +0300

    Maximum average subarray 1, solution 3

--- src/javasolutionsforalgorithms/revision_v2/leetcode/subarray/MaximumAverageSubArray1.java
+++ src/javasolutionsforalgorithms/revision_v2/leetcode/subarray/MaximumAverageSubArray1.java
@@ -14,6 +14,24 @@ package javasolutionsforalgorithms.revision_v2.leetcode.subarray;
  */
 public class MaximumAverageSubArray1 {
 
+    public double findMaxAverage(int[] nums, int k) {
+        int sum = 0;
+        int left = 0;
+        double average = -100000;
+
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+
+            if (right - left + 1 == k) { // if the window size is equal to k then calculate the average. We are doing left + 1 because we are starting from 0
+                average = Math.max(average, (double) sum/k);
+
+                sum -= nums[left];
+                left++;
+            }
+        }
+        return average;
+    }
+
     public double findMaxAverage_good(int[] nums, int k) {
         int sum = 0;
 

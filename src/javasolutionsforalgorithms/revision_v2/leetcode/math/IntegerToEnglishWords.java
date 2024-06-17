9f2603a820a0b5f08763c45ea832d251b3cc0463
mikemacharia39 <mikemacharia39@gmail.com>
2024-06-17 14:48:54 +0300

    IntegerToEnglishWords

--- src/javasolutionsforalgorithms/revision_v2/leetcode/math/IntegerToEnglishWords.java
+++ src/javasolutionsforalgorithms/revision_v2/leetcode/math/IntegerToEnglishWords.java
@@ -21,7 +21,33 @@ package javasolutionsforalgorithms.revision_v2.leetcode.math;
  * 0 <= num <= 2^31 - 1
  */
 public class IntegerToEnglishWords {
-    public String numberToWords(int num) {
+    private final String[] belowTen = new String[] {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    private final String[] belowTwenty = new String[] {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private final String[] belowHundred = new String[] {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
 
+    public String numberToWords(long num) {
+        if (num == 0) return "Zero";
+        return helper(num);
+    }
+
+    private String helper(long num) {
+        String result;
+        if (num < 10) result = belowTen[(int) num];
+        else if (num < 20) result = belowTwenty[(int) (num -10)];
+        else if (num < 100) result = belowHundred[(int) (num/10)] + " " + helper(num % 10);
+        else if (num < 1000) result = helper(num/100) + " Hundred " +  helper(num % 100);
+        else if (num < 1000000) result = helper(num/1000) + " Thousand " +  helper(num % 1000);
+        else if (num < 1000000000) result = helper(num/1000000) + " Million " +  helper(num % 1000000);
+        else result = helper(num/1000000000) + " Billion " + helper(num % 1000000000);
+        return result.trim();
+    }
+
+    public static void main(String[] args) {
+        IntegerToEnglishWords integerToEnglishWords = new IntegerToEnglishWords();
+        Long[] numbers = new Long[]{9L, 12L, 123L, 12345L, 1234567L, 1234567891L, 911234007890L};
+
+        for (Long number : numbers) {
+            System.out.println("Number: " + number + " -> " + integerToEnglishWords.numberToWords(number));
+        }
     }
 }

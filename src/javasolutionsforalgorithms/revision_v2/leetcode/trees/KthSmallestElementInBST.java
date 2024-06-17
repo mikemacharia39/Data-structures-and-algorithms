f8ede9dc3d8a8110ea7294a1f1db0df211393495
mikemacharia39 <mikemacharia39@gmail.com>
2024-06-17 15:20:46 +0300

    Kth smallest element, solution

--- src/javasolutionsforalgorithms/revision_v2/leetcode/trees/KthSmallestElementInBST.java
+++ src/javasolutionsforalgorithms/revision_v2/leetcode/trees/KthSmallestElementInBST.java
@@ -1,10 +1,13 @@
 package javasolutionsforalgorithms.revision_v2.leetcode.trees;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
  */
 public class KthSmallestElementInBST {
-    static class TreeNode {
+    private static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
@@ -13,12 +16,36 @@ public class KthSmallestElementInBST {
     }
 
     /**
-     *
+     * To find the kth smallest element in a BST, we can do an inorder traversal and then return the kth element
+     * Elements in a BST are sorted in an inorder traversal
      * @param root
      * @param k
      * @return
      */
     public int kthSmallest(TreeNode root, int k) {
+        if (root == null) {
+            return -1;
+        }
+        List<Integer> nodes = new ArrayList<>();
+        inorder(root, nodes);
+        return nodes.get(k - 1);
+    }
 
+    private void inorder(TreeNode root, List<Integer> nodes) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, nodes);
+        nodes.add(root.val);
+        inorder(root.right, nodes);
+    }
+
+    public static void main(String[] args) {
+        KthSmallestElementInBST obj = new KthSmallestElementInBST();
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(7);
+        root.left.right = new TreeNode(4);
+        System.out.println(obj.kthSmallest(root, 4));
     }
 }

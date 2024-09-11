881c94ea1fc390c32271b825337ddea91b16140f
mikemacharia39 <mikemacharia39@gmail.com>
2024-09-11 22:50:34 +0300

    Bill division, link

--- src/kotlinsolutionsforalgorithms/hackerrank/implementation/BillDivision.kt
+++ src/kotlinsolutionsforalgorithms/hackerrank/implementation/BillDivision.kt
@@ -1,7 +1,12 @@
 package kotlinsolutionsforalgorithms.hackerrank.implementation
 
+/**
+ * https://www.hackerrank.com/challenges/bon-appetit/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+ */
 class BillDivision {
-
+    //bill: an array of integers representing the cost of each item ordered
+    //k: an integer representing the zero-based index of the item Anna doesn't eat
+    //b: the amount of money that Anna contributed to the bill
     fun bonAppetit(bill: Array<Int>, k: Int, b: Int): Unit {
         var sum = 0
         for (i in bill.indices) {

029e02face17656fa88ebf4498bd72341ec9ebae
mikemacharia39 <mikemacharia39@gmail.com>
2024-09-11 22:49:05 +0300

    Bill division

--- src/kotlinsolutionsforalgorithms/hackerrank/implementation/BillDivision.kt
+++ src/kotlinsolutionsforalgorithms/hackerrank/implementation/BillDivision.kt
@@ -1,4 +1,28 @@
 package kotlinsolutionsforalgorithms.hackerrank.implementation
 
 class BillDivision {
+
+    fun bonAppetit(bill: Array<Int>, k: Int, b: Int): Unit {
+        var sum = 0
+        for (i in bill.indices) {
+            if (i != k) {
+                sum += bill[i]
+            }
+        }
+        sum /= 2
+        if (sum == b) {
+            println("Bon Appetit")
+        } else {
+            println(b - sum)
+        }
+    }
+
+    fun bonAppetit2(bill: Array<Int>, k: Int, b: Int): Unit {
+        val sum = (bill.sum() - bill[k]) / 2
+        if (sum == b) {
+            println("Bon Appetit")
+        } else {
+            println(b - sum)
+        }
+    }
 }

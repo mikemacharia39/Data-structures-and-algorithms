610b6add3be79dc3e4372b617d06671c7daeb05e
mikemacharia39 <mikemacharia39@gmail.com>
2025-01-07 15:34:47 +0300

    revising

--- src/javasolutionsforalgorithms/projecteuler/numbers/SummationOfPrimes.java
+++ src/javasolutionsforalgorithms/projecteuler/numbers/SummationOfPrimes.java
@@ -4,6 +4,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
+ * A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself.
  * https://www.mathblog.dk/project-euler-solutions/
  *
  * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
@@ -26,7 +27,11 @@ public class SummationOfPrimes {
         if (num <= 1) {
             return false;
         }
-        
+        // We do Math.sqrt(num) to reduce the number of iterations, because if a number is not a prime
+        // it will have a factor less than or equal to its square root
+        // e.g. 100, the factors are 1, 2, 4, 5, 10, 20, 25, 50, 100
+        // 100 is not a prime number because it has factors less than or equal to its square root
+        // e.g. 17, the factors are 1, 17
         for (long i = 2; i <= Math.sqrt(num); i++) {
             if (num%i == 0) {
                 return false;

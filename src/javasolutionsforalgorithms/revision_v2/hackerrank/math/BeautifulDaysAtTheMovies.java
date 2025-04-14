194d03421da508525bdfa2a5f832b8996764c40c
mikemacharia39 <mikemacharia39@gmail.com>
2025-04-14 23:39:25 +0300

    Beautiful days, link

--- src/javasolutionsforalgorithms/revision_v2/hackerrank/math/BeautifulDaysAtTheMovies.java
+++ src/javasolutionsforalgorithms/revision_v2/hackerrank/math/BeautifulDaysAtTheMovies.java
@@ -1,10 +1,11 @@
 package javasolutionsforalgorithms.revision_v2.hackerrank.math;
 
 /**
+ * <a href="https://www.hackerrank.com/challenges/beautiful-days-at-the-movies/problem">...</a>
  * Lily likes to play games with integers. She has created a new game where she determines the difference between a number and its reverse.
  * For instance, given the number 12, its reverse is 21. The difference between 12 and 21 is 9.
  * She decides to apply her game to decision making. She will look at a numbered range of days and will only go to a movie on a beautiful day.
- *
+ * <p>
  * Given a range of numbered days, i...j,  and a number k, determine the number of days in the range that are beautiful.
  * Beautiful numbers are defined as numbers where |i-reversed-i|  is evenly divisible by k.
  * If a day's value is a beautiful number, it is a beautiful day. Return the number of beautiful days in the range.

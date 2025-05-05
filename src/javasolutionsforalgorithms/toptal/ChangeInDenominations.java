67b959872003cb332def3dc0cce8cc55f040b06f
mikemacharia39 <mikemacharia39@gmail.com>
2025-05-05 20:05:54 +0300

    change in denominations

--- src/javasolutionsforalgorithms/toptal/ChangeInDenominations.java
+++ src/javasolutionsforalgorithms/toptal/ChangeInDenominations.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
  * Assume that the vending machine will always want to return the least number of coins or notes.
  * Devise a function getChange(M, P) where M is how much money was inserted into the machine and P the price of the item selected,
  * that returns an array of integers representing the number of each denomination to return.
- *
+ * <p>
  * Example:
  * getChange(5, 0.99) 4.01 // should return [1,0,0,0,0,4]
  */
@@ -21,8 +21,9 @@ public class ChangeInDenominations {
         int whole = wholeNoDecimal/100;
         int cents = wholeNoDecimal%100;
 
-        int[] den = {1,5,10,25,50};
-        int[] ans = new int[den.length];
+        //int[] den = {1,5,10,25,50,100};
+        int numberOfDenominations = 6;
+        int[] ans = new int[numberOfDenominations];
 
         while (cents > 0) {
             if (cents >= 50) {
@@ -43,73 +44,7 @@ public class ChangeInDenominations {
             }
         }
 
-        ans[den.length-1] = whole;
-
-        return ans;
-    }
-
-    /**
-     * Original Ans
-     * @param money
-     * @param price
-     * @return
-     */
-    public int[] getChange3(double money, double price) {
-        double change = money - price;
-        // 4.01 get whole number
-        int wholeNoDecimal = (int)(change*100);
-        int whole = wholeNoDecimal/100;
-        int cents = wholeNoDecimal%100;
-
-        System.out.println(cents);
-
-        int[] den = {1,5,10,25,50};
-        int[] ans = new int[den.length];
-
-        while (cents > 0) {
-
-            for (int i = den.length-1; i >= 1; i--) {
-                if (den[i] >= cents) {
-                    cents -= den[i];
-                    ans[den.length - i-1]++;
-                }
-
-                if (cents <= 0) {
-                    break;
-                }
-            }
-        }
-
-        ans[den.length-1] = whole;
-
-        return ans;
-    }
-
-    /**
-     * Original Ans
-     * @param money
-     * @param price
-     * @return
-     */
-    public int[] getChange2(double money, double price) {
-        double change = money - price;
-        // 4.01 get whole number
-        int whole = (int) change;
-        double cents = change - whole;
-
-        int[] den = {1,5,10,25,50};
-        int[] ans = new int[den.length];
-
-        while (cents > 0) {
-            for (int i = den.length-1; i >= 1; i--) {
-                if (den[i] >= cents) {
-                    cents -= den[i];
-                    ans[i]++;
-                }
-            }
-        }
-
-        ans[den.length-1] = whole;
+        ans[numberOfDenominations-1] = whole; // the last
 
         return ans;
     }
@@ -118,6 +53,7 @@ public class ChangeInDenominations {
         ChangeInDenominations changeInDenominations = new ChangeInDenominations();
         double money = 5;
         double price = 0.89;
+        // balance = 4.11
         System.out.println(Arrays.toString(changeInDenominations.getChange(money, price)));
     }
 }

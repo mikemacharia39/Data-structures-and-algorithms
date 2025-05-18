ca2752b8da875c891d5cc9901242d33e4f571fce
mikemacharia39 <mikemacharia39@gmail.com>
2025-05-18 17:50:45 +0300

    IntersectionOfTwoArraysTwo solution 2

--- src/javasolutionsforalgorithms/revision_v2/leetcode/arrays/IntersectionOfTwoArraysTwo.java
+++ src/javasolutionsforalgorithms/revision_v2/leetcode/arrays/IntersectionOfTwoArraysTwo.java
@@ -1,5 +1,10 @@
 package javasolutionsforalgorithms.revision_v2.leetcode.arrays;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class IntersectionOfTwoArraysTwo {
     public int[] intersect1(int[] nums1, int[] nums2) {
         int n = nums1.length;
@@ -20,6 +25,28 @@ public class IntersectionOfTwoArraysTwo {
         int[] result = new int[index];
         for(int i = 0; i < index; i++){
             result[i] = temp[i];
+        }
+        return result;
+    }
+
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> n1Map = new HashMap<>();
+
+        for (int num : nums1) {
+            n1Map.put(num, n1Map.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> resultList = new ArrayList<>();
+        for (int num : nums2) {
+            if (n1Map.getOrDefault(num, 0) > 0) {
+                resultList.add(num);
+                n1Map.put(num, n1Map.get(num) - 1);
+            }
+        }
+
+        int[] result = new int[resultList.size()];
+        for (int i = 0; i < resultList.size(); i++) {
+            result[i] = resultList.get(i);
         }
         return result;
     }

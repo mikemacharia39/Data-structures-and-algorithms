93d7060a175025e6e46babd64198f88222480415
mikemacharia39 <mikemacharia39@gmail.com>
2025-05-20 14:17:05 +0300

    container with max water

--- src/javasolutionsforalgorithms/leettests/interestingquestions/ContainerWithMostWater.java
+++ src/javasolutionsforalgorithms/leettests/interestingquestions/ContainerWithMostWater.java
@@ -76,4 +76,11 @@ public class ContainerWithMostWater {
 
         return maxArea;
     }
+
+    public static void main(String[] args) {
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        ContainerWithMostWater containerWithMostWater = new ContainerWithMostWater();
+        int water = containerWithMostWater.maxArea(height);
+        System.out.println("Max Water: " + water);
+    }
 }

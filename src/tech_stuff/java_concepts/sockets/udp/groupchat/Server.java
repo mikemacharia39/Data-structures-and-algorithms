29b0113a6dd316af3896579b74e460a9dd745387
mikemacharia39 <mikemacharia39@gmail.com>
2025-12-08 20:12:54 +0300

    use separate thread for broadcasting message

--- src/tech_stuff/java_concepts/sockets/udp/groupchat/Server.java
+++ src/tech_stuff/java_concepts/sockets/udp/groupchat/Server.java
@@ -12,27 +12,43 @@ import java.util.Set;
 public class Server {
     private static final int PORT = 9876;
     private static final int BUFFER_SIZE = 1024;
-    private static Set<SocketAddress> clients = Collections.synchronizedSet(new HashSet<>());
+    private static final Set<SocketAddress> clients = Collections.synchronizedSet(new HashSet<>());
 
     public static void main(String[] args) throws IOException {
-        DatagramSocket socket = new DatagramSocket(PORT);
-        byte[] buffer = new byte[BUFFER_SIZE];
-        System.out.println("UDP Group Chat Server started on port " + PORT);
+        try (DatagramSocket socket = new DatagramSocket(PORT)) {
+            byte[] buffer = new byte[BUFFER_SIZE];
+            System.out.println("UDP Group Chat Server started on port " + PORT);
 
-        while (true) {
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-            socket.receive(packet);
+            while (true) {
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packet);
 
-            // Add client to the set
-            clients.add(packet.getSocketAddress());
+                // Add client to the set
+                clients.add(packet.getSocketAddress());
 
-            String msg = new String(packet.getData(), 0, packet.getLength());
-            System.out.println("Received: " + msg + " from " + packet.getSocketAddress());
+                String msg = new String(packet.getData(), 0, packet.getLength());
+                System.out.println("Received: " + msg + " from " + packet.getSocketAddress());
 
-            // Broadcast to all clients
+                // Broadcast to all clients in separate thread
+                Thread.startVirtualThread(() -> {
+                    try {
+                        broadcastMessage(socket, msg);
+                    } catch (IOException e) {
+                        System.err.println("Error broadcasting message: " + e.getMessage());
+                    }
+                });
+            }
+        } catch (IOException e) {
+            System.err.println("Server error: " + e.getMessage());
+        }
+    }
+
+    private static void broadcastMessage(DatagramSocket socket, String message) throws IOException {
+        byte[] data = message.getBytes();
+        synchronized (clients) {
             for (SocketAddress client : clients) {
                 DatagramPacket outPacket = new DatagramPacket(
-                    msg.getBytes(), msg.length(), ((InetSocketAddress) client).getAddress(), ((InetSocketAddress) client).getPort()
+                        data, data.length, ((InetSocketAddress) client).getAddress(), ((InetSocketAddress) client).getPort()
                 );
                 socket.send(outPacket);
             }

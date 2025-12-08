d562a8e549577a16b7fcbe15a731b7982b1503f1
mikemacharia39 <mikemacharia39@gmail.com>
2025-12-08 16:25:51 +0300

    Socket programming

---
+++ src/tech_stuff/java_concepts/sockets/Client.java
@@ -0,0 +1,32 @@
+package tech_stuff.java_concepts.sockets;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class Client {
+
+    public static void main(String[] args) {
+        System.out.println("Starting Client...");
+        try {
+            Socket clientSocket = new Socket("127.0.0.1", 9999);
+
+            System.out.println("Connected to server: " + clientSocket.getInetAddress().getHostAddress() + ":" + clientSocket.getPort());
+
+            InputStream inputStream = clientSocket.getInputStream();
+            OutputStream outputStream = clientSocket.getOutputStream();
+
+            outputStream.write("Hello World From Client".getBytes());
+
+            byte[] bufferResponse = new byte[1024]; // 1024 bytes buffer
+            inputStream.read(bufferResponse);
+
+            System.out.println("Response from server: " + new String(bufferResponse).trim());
+
+            clientSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
